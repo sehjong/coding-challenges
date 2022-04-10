2bdd3abdb7f544bcdad7ea4661af62d96b728f97
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-04-10 06:11:42 -0500

    add details to coding challenge for 2022-04-12

---
+++ cc-2022-04-12/js/main.js
@@ -0,0 +1,15 @@
+// PREP
+
+// Write a function which converts the input string to uppercase.
+
+// P: a string with letters that are either lowercase, uppercase, or both lowercase and uppercase
+// R: the string to all uppercase letters
+// E: 'hello' => 'HELLO'
+//    'AND' => 'AND'
+//    'gOodByE' => 'GOODBYE'
+// P:
+
+// create a function that takes in a string of letters
+function makeUpperCase(str) {
+    
+}
