269b819d9d79acafe595acc110f9e07788efda74
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-04-13 20:46:40 -0500

    add details to coding challenge for 2022-04-15

---
+++ cc-2022-04-15/js/main.js
@@ -0,0 +1,14 @@
+// PREP
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// P: a string
+// R: the string without exclamation marks 
+// E: 'hello!' => 'hello'
+//    'hello!!' => 'hello'
+// P:
+
+// create a function that takes in a string
+function removeExclamationMarks(str) {
+
+}
