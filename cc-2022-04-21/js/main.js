aa90ae0053fd992a668da26c4a2b044c8eddb5c5
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-04-19 17:54:56 -0500

    add details to coding challenge for 2022-04-21

---
+++ cc-2022-04-21/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+// Palindrome strings
+// A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This includes capital letters, punctuation, and word dividers.
+
+// Implement a function that checks if something is a palindrome. If the input is a number, convert it to string first.
+
+// Examples(Input ==> Output)
+// "anna"   ==> true
+// "walter" ==> false
+// 12321    ==> true
+// 123456   ==> false
+
+// P: string, if number, convert to string first
+// R: whether word is a palindrome, return true or false
+// E: "anna"   ==> true
+//    "walter" ==> false
+//    12321    ==> true
+//    123456   ==> false
+// P:
+
+// create a function that takes in a word or number
+function checkPalindrome(input) {
+
+}
