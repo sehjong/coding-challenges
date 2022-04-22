3d35074f7eeb7311de13f5f58e7849a26049b598
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-04-22 15:45:32 -0500

    add details to coding challenge for 2022-04-24

---
+++ cc-2022-04-24/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+// Examples
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+// P: whole numbers
+// R: a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive )
+// E: n = 0  ==> [1]        # [2^0]
+//    n = 1  ==> [1, 2]     # [2^0, 2^1]
+//    n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+// P:
+
+// create a function that takes a non-negative integer n as input
+function powersOfTwo(n) {
+
+}
