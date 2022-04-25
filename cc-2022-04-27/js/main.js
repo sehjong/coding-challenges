62ffa34ed012c7f0d315b49cff3e16205e1ad851
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-04-25 11:03:06 -0500

    add details to coding challenge for 2022-04-27

---
+++ cc-2022-04-27/js/main.js
@@ -0,0 +1,15 @@
+// PREP
+
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+// P: integer
+// R: which quarter of the year a month given as an integer belongs as an integer
+// E: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter
+// P:
+
+// create a function that takes in a number
+function quarterOf(month) {
+    
+}
