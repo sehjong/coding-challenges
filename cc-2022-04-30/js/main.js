11d869bd4a35e79778b060db9335173e004ebc8a
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-04-28 13:25:26 -0500

    add details to coding challenge for 2022-04-30

---
+++ cc-2022-04-30/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+// P: array of integers, if the input is an empty array or is null, return an empty array, 0 is neither positive nor negative
+// R: an array where the first element is the count of positive numbers and the second element is the sum of negative numbers
+// E: For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65]
+// P:
+
+// create a function that takes in an array of numbers
+function countPositivesSumNegatives(input) {
+
+}
