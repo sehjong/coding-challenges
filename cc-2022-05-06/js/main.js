79c0f681a9ae560a8f44be799596e9e38a87a39f
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-05-04 12:58:28 -0500

    add details to coding challenge for 2022-05-06

---
+++ cc-2022-05-06/js/main.js
@@ -0,0 +1,21 @@
+// PREP
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+// P: integers
+// R: an array of all integers between the input parameters, including them
+// E: a = 1
+//    b = 4
+//    --> [1, 2, 3, 4]
+// P:
+
+// create a function that takes two integers (a, b, where a < b)
+function between(a, b) {
+    
+}
