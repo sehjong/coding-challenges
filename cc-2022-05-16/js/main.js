3a844c6cc4e6d1e3eab43905b9250b88f45c64b0
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-05-14 20:17:40 -0500

    add details to coding challenge for 2022-05-16

---
+++ cc-2022-05-16/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+// P: numbers, strings, is the array ever empty? undefined?
+// R: TRUE if the array contains the value, FALSE if the array does not contain the value
+// E: given [1, 2, 3] as A and 4 as X, should return FALSE
+//    given [4, 5, 6] as A and 4 as X, should return TRUE
+//    given [] as A and 4 as X, should return FALSE
+// P: 
+
+// create a function that takes in two parameters: an array and a random number
+function checkArr(arr, num) {
+    
+}
