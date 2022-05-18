9a33338caa0e3f109dbc79fb9b853be472d09335
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-05-18 18:12:27 -0500

    add details to coding challenge for 2022-05-20

---
+++ cc-2022-05-20/js/main.js
@@ -0,0 +1,16 @@
+// PREP
+
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+// P: numbers only, integers, if there is nothing to sum, the sum is default to 0
+// R: the sum of all of the positive numbers
+// E: [1,-4,7,12] => 1 + 7 + 12 = 20
+// P:
+
+// create a function that takes in an array of numbers
+// loop through the array
+// return the sum
