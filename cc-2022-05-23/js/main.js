a86f03e11bd542867f799bd6a08f27eea4cdb549
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-05-21 14:32:04 -0500

    add details to coding challenge for 2022-05-23

---
+++ cc-2022-05-23/js/main.js
@@ -0,0 +1,14 @@
+// PREP
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+// P: array
+// R: string without spaces
+// E: ['s', 't', 'r', 'i', 'n', 'g'] => ['string']
+// P:
+
+// create a function that takes in an array
+function strArr(arr) {
+// return the string without spaces
+    return
+}
