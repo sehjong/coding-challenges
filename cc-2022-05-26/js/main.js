44343933658531f9fe08bd960b5e13c631372a28
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-05-24 10:09:15 -0500

    add details to coding challenge for 2022-05-26

---
+++ cc-2022-05-26/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+// P: string of words, array of words
+// R: an array of words
+// E: "Robin Singh" ==> ["Robin", "Singh"], "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+// P:
+
+// create a function that takes in a string of words
+function strToArr(str) {
+// return an array of words
+    return 
+}
