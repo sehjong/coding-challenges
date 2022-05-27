2a48ef69a9c337a6fe01b5b98644ee4355b2d534
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-05-27 11:35:14 -0500

    add details to coding challenge for 2022-05-29

---
+++ cc-2022-05-29/js/main.js
@@ -0,0 +1,16 @@
+// PREP
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+// P: integers as strings and numbers
+// R: the sum of the array values as if all were numbers
+// E: ['4', '2', '11', '61'] => 78
+// P:
+
+// create a function that takes in an array of integers as strings and numbers
+function sumArrStrNums(arr) {
+// return the sum of the array values as if all were numbers
+    return 
+}
