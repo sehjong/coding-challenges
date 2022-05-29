f2303696b6a17e3603fac1648e16c60880741208
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-05-29 11:49:18 -0500

    add solution to coding challenge for 2022-05-29

--- cc-2022-05-29/js/main.js
+++ cc-2022-05-29/js/main.js
@@ -12,5 +12,5 @@
 // create a function that takes in an array of integers as strings and numbers
 function sumArrStrNums(arr) {
 // return the sum of the array values as if all were numbers
-    return arr.map();
+    return arr.reduce((acc, c) => Number(acc) + Number(c), 0);
 }
