5e07f880a4fbed4188cb22c6f48c0d2ec7b0a8df
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-05-30 12:37:45 -0500

    add details to coding challenge for 2022-06-01

---
+++ cc-2022-06-01/js/main.js
@@ -0,0 +1,16 @@
+// PREP
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+// P: integers
+// R: the sum of squares
+// E: [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9
+// P:
+
+// create a function that takes in an array of numbers
+function sumArrSquared(arr) {
+// return the sum of the squared numbers   
+    return 
+}
