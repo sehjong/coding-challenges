6ffdefa1be5c806fd74774eb0a0fe5ef21749abc
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-06-03 21:20:35 -0500

    add attempt to coding challenge for 2022-06-04

--- cc-2022-06-04/js/main.js
+++ cc-2022-06-04/js/main.js
@@ -18,5 +18,7 @@
 
 // create a function with two arguments that will return an array of the first (n) multiples of (x)
 function countBy(x, n) {
-
+    let arr = [];
+// return the results as an array
+    return arr;
 }
