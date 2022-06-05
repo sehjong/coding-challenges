904299786ca2d1e18f9e4cce87c67bb8f581f30c
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-06-05 08:12:28 -0500

    add details to coding challenge for 2022-06-07

---
+++ cc-2022-06-07/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+// P: integers or floating-point numbers
+// R: its opposite
+// E: 1: -1
+//    14: -14
+//    -34: 34
+// P:
+
+// create a function that takes in a number
+function oppositeNum(num) {
+// return its opposite
+    return
+}
