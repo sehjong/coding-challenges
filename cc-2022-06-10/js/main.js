1ecfd51e2d1f85d2f5f6c5ae40ee537a79fbee2c
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-06-08 10:58:18 -0500

    add details to coding challenge for 2022-06-10

---
+++ cc-2022-06-10/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+// P: integers
+// R: a string
+// E: 123 --> "123"
+//    999 --> "999"
+// P:
+
+// create a function that takes in a number
+function numToStr(num) {
+    
+}
