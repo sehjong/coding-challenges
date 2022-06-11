7db94248896adaca2bf8705344afce640d8b9ec4
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-06-11 11:51:54 -0500

    add details to coding challenge for 2022-06-13

---
+++ cc-2022-06-13/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+// P: words only
+// R: reversed string
+// E: 'world'  =>  'dlrow'
+//     'word'   =>  'drow'
+// P:
+
+// create a function that reverses the string passed into it
+function reverseStr(str) {
+
+}
