83273a2ff1c3b0064f344d887cec64752619fe69
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-06-14 12:24:51 -0500

    add details to coding challenge for 2022-06-16

---
+++ cc-2022-06-16/js/main.js
@@ -0,0 +1,18 @@
+// PREP
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+// P: words and integers
+// R: a string of s repeated exactly n times
+// E: 6, "I"     -> "IIIIII"
+//    5, "Hello" -> "HelloHelloHelloHelloHello"
+// P:
+
+// create a function that takes in an integer n and a string s as parameters
+function repeatStrNum(num, str) {
+    
+}
