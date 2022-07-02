7d1dfa352ffa225649c9567239f49e7a6e7c6b6d
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-07-02 14:01:08 -0500

    add details to coding challenge for 2022-07-04

---
+++ cc-2022-07-04/js/main.js
@@ -0,0 +1,26 @@
+// PREP
+
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+// P: integers
+// R: Given a year, return the century it is in.
+// E: 1705 --> 18
+//    1900 --> 19
+//    1601 --> 17
+//    2000 --> 20
+// P:
+
+// create a function century that takes in a number year
+function century(year) {
+
+}
