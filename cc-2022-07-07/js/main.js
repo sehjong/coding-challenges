0a43c6f9edcbef8bfe89a3935eefd99db6744c8b
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-07-05 14:56:31 -0500

    add details to coding challenge for 2022-07-07

---
+++ cc-2022-07-07/js/main.js
@@ -0,0 +1,26 @@
+// PREP
+
+// Summation
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+// P: The number will always be a positive integer greater than 0.
+// R: the summation of every number from 1 to num
+// E: summation(2) -> 3
+//    1 + 2
+
+//    summation(8) -> 36
+//    1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+// P:
+
+// create a program that finds the summation of every number from 1 to num
+var summation = function (num) {
+    
+}
