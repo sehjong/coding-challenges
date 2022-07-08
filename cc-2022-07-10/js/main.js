40d16360657a788f962a3d082658005458072a04
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-07-08 16:03:35 -0500

    add details to coding challenge for 2022-07-10

---
+++ cc-2022-07-10/js/main.js
@@ -0,0 +1,23 @@
+// PREP
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+// P: words
+// R: two capital letters with a dot separating them
+// E: Sam Harris => S.H
+
+//    patrick feeney => P.F
+// P:
+
+// create a function that takes in a string
+function abbrevName(name) {
+    
+}
