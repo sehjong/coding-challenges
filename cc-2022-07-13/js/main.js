b4beb0a57282c9641006de82642827e0525084e5
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-07-11 16:54:14 -0500

    add details to coding challenge for 2022-07-13

---
+++ cc-2022-07-13/js/main.js
@@ -0,0 +1,15 @@
+// PREP
+
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// P: integers
+// R: the result of multiplying the values together
+// E: [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+// P:
+
+// create a function that takes in an array of numbers
+function multiplyArr(intArr) {
+
+}
