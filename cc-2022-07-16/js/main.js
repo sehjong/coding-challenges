21d3429230f8ea407187487151cb0446337a174a
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-07-14 18:04:14 -0500

    add details to coding challenge for 2022-07-16

---
+++ cc-2022-07-16/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+
+// Examples:
+// 1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+// 2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+// 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+// 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+
+// P: numbers are positive, non-zero integers
+// R: checks if a number n is divisible by two numbers x AND y
+// E: 1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+//    2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+//    3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+//    4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+// P:
+
+// create a function that takes in numbers n, x, and y
+function isDivisible(n, x, y) {
+
+}
