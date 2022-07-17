ab6d7c14046825eb2102aec7c1fbee078a94bbbc
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-07-17 12:48:17 -0500

    add details to coding challenge for 2022-07-19

---
+++ cc-2022-07-19/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+// P: integers
+// R: the additive inverse
+// E: invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+//    invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+//    invert([]) == []
+// P:
+
+// create a function that takes in an array of numbers
+function inverse(intArr) {
+    
+}
