bf130adcb81e6ffca59c9c86365221a4ff0008b0
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-07-23 17:07:49 -0500

    add details to coding challenge for 2022-07-25

---
+++ cc-2022-07-25/js/main.js
@@ -0,0 +1,23 @@
+// PREP
+
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// For example, if this array were passed as an argument:
+
+// ["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+// Your function would return the following array:
+
+// ["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+// All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+// P: order an array of strings based on the length of those strings from shortest to longest, all of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length
+// R: a sorted array containing strings, ordered from shortest to longest
+// E: ["Telescopes", "Glasses", "Eyes", "Monocles"] => ["Eyes", "Glasses", "Monocles", "Telescopes"]
+// P:
+
+// create a function that takes in an array of strings
+function arrStr(strArr) {
+
+}
