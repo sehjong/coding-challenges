ffec8932ee9cdac2de456770e85aff2e0a529892
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-07-30 19:09:20 -0500

    add attempt to coding challenge for 2022-07-31

--- cc-2022-07-31/js/main.js
+++ cc-2022-07-31/js/main.js
@@ -20,5 +20,8 @@
 
 // create a function that takes in a string of space separated numbers
 function highAndLow(numbers) {
-    
+    const arr = numbers.split('').sort(function (a, b) {
+        return a - b;
+    });
+    return `${arr[arr.length - 1]} ${arr[0]}`;
 }
