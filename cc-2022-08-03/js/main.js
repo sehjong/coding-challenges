b1aee8004d08ccc173ff2cae6a28f809af7dc27e
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-08-01 19:27:48 -0500

    add details to coding challenge for 2022-08-03

---
+++ cc-2022-08-03/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+// P: array of integers
+// R: an array of even numbers
+// E: getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+// P:
+
+// create a function that takes in an array of numbers
+function getEvenNumbers(intArr) {
+
+}
