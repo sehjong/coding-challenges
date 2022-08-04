7e01852e85794f79770d321f7a17e121f2f0f937
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-08-04 17:52:39 -0500

    add details to coding challenge for 2022-08-06

---
+++ cc-2022-08-06/js/main.js
@@ -0,0 +1,18 @@
+// PREP
+
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+// P: string of words, including spaces
+// R: reverses each word in the string, all spaces in the string should be retained
+// E: "This is an example!" ==> "sihT si na !elpmaxe"
+//    "double  spaces"      ==> "elbuod  secaps"
+// P:
+
+// create a function that takes in a string of words
+function reverseWords(str) {
+    
+}
