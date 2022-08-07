5f5861be2ce8e95ae0f8dacee558dfb5a3ac53a0
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-08-07 13:18:37 -0500

    add details to coding challenge for 2022-08-09

---
+++ cc-2022-08-09/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+// P: natural numbers, whole numbers
+// R: digits in descending order
+// E: Input: 42145 Output: 54421
+//    Input: 145263 Output: 654321
+//    Input: 123456789 Output: 987654321
+// P:
+
+// create a function that takes in any natural or whole number
+function descendingOrder(num) {
+
+}
