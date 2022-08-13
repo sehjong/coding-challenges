055d575223f8037956b832ed06a6253a468281e4
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-08-13 12:27:14 -0500

    add details to coding challenge for 2022-08-15

---
+++ cc-2022-08-15/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+// P: whole numbers and strings
+// R: a new array with the strings filtered out
+// E: filter_list([1,2,'a','b']) == [1,2]
+//    filter_list([1,'a','b',0,15]) == [1,0,15]
+//    filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+// P:
+
+// create a function that takes in whole numbers and strings
+function filter_list(listArr) {
+
+}
