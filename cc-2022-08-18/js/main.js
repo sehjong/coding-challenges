9b2952645c8c7c3c47baf3f591e4735b9af902f8
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-08-16 08:49:21 -0500

    add details to coding challenge for 2022-08-18

---
+++ cc-2022-08-18/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// Given a sequence of numbers, find the largest pair sum in the sequence.
+
+// For example
+
+// [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+// [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+// Input sequence contains minimum two elements and every element is an integer.
+
+// P: an array of numbers, contains minimum two elements and every element is an integer
+// R: the largest pair sum from the array
+// E: [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+//    [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+// P:
+
+// create a function that takes in an array of numbers
+function largestPairSum(numbers) {
+
+}
