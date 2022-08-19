c4e0f391674e760b6d7f788a49a2124dec16427a
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-08-19 09:38:29 -0500

    add details to coding challenge for 2022-08-21

---
+++ cc-2022-08-21/js/main.js
@@ -0,0 +1,18 @@
+// PREP
+
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+// P: an array of whole numbers, no floats, minimum of 4 whole numbers in the array
+// R: the sum of the two lowest positive numbers given an array of minimum 4 positive integers
+// E: [19, 5, 42, 2, 77], the output should be 7
+//    [10, 343445353, 3453445, 3453545353453] should return 3453455
+// P:
+
+// create a function that takes in an array of numbers
+function sumTwoSmallestNumbers(numbers) {
+
+}
