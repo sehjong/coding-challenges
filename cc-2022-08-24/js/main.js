6098baa6c65c5ca74cc56da2650922750e5f2ab0
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-08-22 11:09:21 -0500

    add details to coding challenge for 2022-08-24

---
+++ cc-2022-08-24/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+// Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+// Examples
+//  123 ->  321
+// -456 -> -654
+// 1000 ->    1
+
+// P: integers, numbers should preserve their sign; i.e. a negative number should still be negative when reversed
+// R: output its reverse digits
+// E: 123 ->  321
+//    -456 -> -654
+//    1000 ->    1
+// P:
+
+// create a function that takes in a number
+function reverseNumber(n) {
+    
+}
