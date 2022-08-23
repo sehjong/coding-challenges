4419697d369cd5c0562ec98af71b10bc5b2235fd
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-08-23 11:16:02 -0500

    add attempt to coding challenge for 2022-08-24

--- cc-2022-08-24/js/main.js
+++ cc-2022-08-24/js/main.js
@@ -18,5 +18,8 @@
 
 // create a function that takes in a number
 function reverseNumber(n) {
-    
+    let isNegative = n < 0;
+    let reverseAsString = Math.abs(n).toString().split(' ').reverse().join('');
+    let result = Number(reverseAsString);
+    return isNegative ? -result : result;
 }
