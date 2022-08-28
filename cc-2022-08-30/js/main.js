db5bd121838902fd3bcbfb74cfb96e03cf9edda4
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-08-28 16:14:22 -0500

    add details to coding challenge for 2022-08-30

---
+++ cc-2022-08-30/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+// P: string, can contain any character
+// R: a boolean to see if a string has the same amount of 'x's and 'o's, be case insensitive, and should return true when no 'x' and 'o' is present
+// E: XO("ooxx") => true
+//    XO("xooxx") => false
+//    XO("ooxXm") => true
+//    XO("zpzpzpp") => true
+//    XO("zzoo") => false
+// P:
+
+// create a function that takes in a string
+function XO(str) {
+
+}
