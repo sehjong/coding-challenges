67112544c832a149e5a63eff7ee8de74a990b4c8
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-08-31 19:06:23 -0500

    add details to coding challenge for 2022-09-02

---
+++ cc-2022-09-02/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+// Write function which will create a string from a list of strings, separated by space.
+
+// Example:
+
+// ["hello", "world"] -> "hello world"
+
+// P: strings only
+// R: a string from a list of strings, separated by space
+// E: ["hello", "world"] -> "hello world"
+// P:
+
+// create a function that takes in words as an array of strings
+function wordsToSentence(words) {
+
+}
