4f96f000c806b55379c3c42ff7b1ef0ad3b5b289
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-09-06 10:39:56 -0500

    add details to coding challenge for 2022-09-08

---
+++ cc-2022-09-08/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+
+// Good luck!
+
+// If you like this Kata, please try:
+
+// Indexed capitalization
+
+// Even-odd disparity
+
+// P: a lowercase string with no spaces
+// R: capitalize the letters that occupy even indexes and odd indexes separately, index 0 will be considered even
+// E: capitalize("abcdef") = ['AbCdEf', 'aBcDeF']
+// P:
+
+// create a function that takes in a string
+function capitalize(s) {
+
+}
