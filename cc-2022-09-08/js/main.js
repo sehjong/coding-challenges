7ac3e63141dcc9e629029011d28c924f44c2d75a
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-09-08 10:51:44 -0500

    add solution to coding challenge for 2022-09-08

--- cc-2022-09-08/js/main.js
+++ cc-2022-09-08/js/main.js
@@ -21,7 +21,7 @@
 
 // create a function that takes in a string
 function capitalize(s) {
-    const odd = s.split(' ').map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join('');
-    const even = s.split(' ').map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join('');
+    const odd = s.split('').map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join('');
+    const even = s.split('').map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join('');
     return [even, odd];
 }
