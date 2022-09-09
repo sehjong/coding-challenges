4a67c4154c45ab6afb37eac2be800b3598bf514d
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-09-09 08:32:19 -0500

    add details to coding challenge for 2022-09-11

---
+++ cc-2022-09-11/js/main.js
@@ -0,0 +1,29 @@
+// PREP
+
+// Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+// Example:
+// For input: [3, 4, 4, 3, 6, 3]
+
+// remove the 3 at index 0
+// remove the 4 at index 1
+// remove the 3 at index 3
+// Expected output: [4, 6, 3]
+
+// More examples can be found in the test cases.
+
+// Good luck!
+
+// P: an array of integers
+// R: the array without duplicates
+// E: For input: [3, 4, 4, 3, 6, 3]
+//    remove the 3 at index 0
+//    remove the 4 at index 1
+//    remove the 3 at index 3
+//    Expected output: [4, 6, 3]
+// P:
+
+// create a function that takes in an array of numbers
+function solve(arr) {
+    
+}
