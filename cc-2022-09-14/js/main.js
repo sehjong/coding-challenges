1d74045b7dfaa7cee6c649dc5010980636b4fcb2
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-09-12 09:11:01 -0500

    add details to coding challenge for 2022-09-14

---
+++ cc-2022-09-14/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+// Note: The function accepts an integer and returns an integer
+
+// P: integers only
+// R: the square of every digit of a number concatenated
+// E: 9119 through the function will return 811181 because 92 is 81 and 12 is 1
+// P:
+
+// create a function that takes in a number
+function squareDigits(num) {
+    
+}
