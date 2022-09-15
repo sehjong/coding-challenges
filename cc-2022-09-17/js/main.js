929ef6d7529319dc129829f494c80f014921cf21
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-09-15 14:06:00 -0500

    add details to coding challenge for 2022-09-17

---
+++ cc-2022-09-17/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+// P: string, only lower case letters and/or spaces
+// R: the number (count) of vowels in the given string
+// E: 'hello' will return 2 because there are two vowels in string 'hello'
+// P:
+
+// create a function that takes in a string
+function getCount(str) {
+    
+}
