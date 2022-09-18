ef85a580792239885c767f76189191a473b061dd
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-09-18 16:54:22 -0500

    add details to coding challenge for 2022-09-20

---
+++ cc-2022-09-20/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+// Example:
+
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+
+// P: string as name of letters only, no spaces
+// R: the string with a greeting, capitalized and ending with an exclamation point
+// E: "riley" --> "Hello Riley!"
+//    "JACK"  --> "Hello Jack!"
+// P:
+
+// extend String.prototype with a method to create a function that takes in a string
+String.prototype.capitalize = function() {
+
+}
