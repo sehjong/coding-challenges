1960f7f33673b66832fc9019204abbfe7b96cd68
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-09-21 20:30:57 -0500

    add details to coding challenge for 2022-09-23

---
+++ cc-2022-09-23/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
+
+// Example:
+
+// * With input "The quick brown fox jumps over the lazy dog", 4
+// * Return ['quick', 'brown', 'jumps']
+
+// P: a string and an integer n as parameters
+// R: a list of all words that are longer than n
+// E: With input "The quick brown fox jumps over the lazy dog", 4
+//    Return ['quick', 'brown', 'jumps']
+// P:
+
+// create a function that takes in a string and an integer n as parameters
+function filterLongWord(sentence, n) {
+    
+}
