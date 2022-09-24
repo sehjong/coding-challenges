ba277e9e5f2d13afc70e473d8ff04b6281b12ac3
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-09-24 11:59:11 -0500

    add details to coding challenge for 2022-09-26

---
+++ cc-2022-09-26/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// Write a function that doubles every second integer in a list, starting from the left.
+
+// Example:
+
+// For input array/list :
+
+// [1,2,3,4]
+// the function should return :
+
+// [1,4,3,8]
+
+// P: an array of numbers, integers
+// R: every second integer in a list doubled, starting from the left
+// E: [1,2,3,4] should return [1,4,3,8]
+// P:
+
+// create a function that takes in an array of numbers
+function doubleEveryOther(intArr) {
+
+}
