5d8fbdf6ee1eaa75af34602ea85eeb3bf559a20f
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-09-28 19:51:55 -0500

    add attempt to coding challenge for 2022-09-29

--- cc-2022-09-29/js/main.js
+++ cc-2022-09-29/js/main.js
@@ -15,5 +15,6 @@
 
 // create a function that takes in an array of numbers and a string 'value' or 'index'
 function min(intArr, toReturn) {
-    
+    let val = Math.min.apply(null, intArr);
+    toReturn == 'value' ? val : intArr.indexOf(val);
 }
