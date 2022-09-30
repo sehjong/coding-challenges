c4a3c3f1a7881e0f4695658bf350fbc6fb1ae9ce
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-09-30 13:23:16 -0500

    add details to coding challenge for 2022-10-02

---
+++ cc-2022-10-02/js/main.js
@@ -0,0 +1,34 @@
+// PREP
+
+// Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+// Have fun!
+
+// P: an array of integers
+// R: string 'odd' or 'even' whether the sum of elements in the array is odd or even
+// E: Input: [0]
+//    Output: "even"
+//    Input: [0, 1, 4]
+//    Output: "odd"
+//    Input: [0, -1, -5]
+//    Output: "even"
+// P:
+
+// create a function that takes in an array of numbers
+function oddOrEven() {
+    
+}
