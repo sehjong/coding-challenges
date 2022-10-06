6a2f58a655dbafa12da6a6465f949509b672633c
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-10-06 10:45:39 -0500

    add details to coding challenge for 2022-10-08

---
+++ cc-2022-10-08/js/main.js
@@ -0,0 +1,13 @@
+// PREP
+
+// Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+
+// P: strings, words only, letters, spaces
+// R: every letter in the string switched from uppercase to lowercase and from lowercase to uppercase, keep word order and spaces
+// E: Hello World -> hELLO wORLD
+// P:
+
+// create a function that takes in a string
+function alternateCase(str) {
+    
+}
