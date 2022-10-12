9d4b266cbdb7e6fb64af79994d814433a876b333
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-10-12 13:55:24 -0500

    add details to coding challenge for 2022-10-14

---
+++ cc-2022-10-14/js/main.js
@@ -0,0 +1,18 @@
+// PREP
+
+// Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+
+// Example:
+
+// keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]]
+// Style Points (JS/CoffeeScript only): This kata only tests for data that uses object literal notation (simple objects). For extra style, can you get your method to check for objects that extend their prototype?
+
+// P: object, properties and values, key value pairs, strings and numbers
+// R: the keys and values as separate arrays
+// E: keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]]
+// P:
+
+// create a function that takes in an object of key value pairs
+function keysAndValues(data) {
+    
+}
