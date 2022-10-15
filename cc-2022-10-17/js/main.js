95e4035f8bbf6f128b6327d76439dc6a0ea83339
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-10-15 09:01:47 -0500

    add details to coding challenge for 2022-10-17

---
+++ cc-2022-10-17/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+// P: natural numbers
+// R: the sum of the numbers in the nth row of this triangle (starting at index 1)
+// E: 1 -->  1
+//    2 --> 3 + 5 = 8
+// P:
+
+// create a function that takes in a number
+function rowSumOddNumbers(num) {
+    
+}
