fd57be8a69e48f92ec9c485b9fec5a0eb8173d23
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-10-24 20:46:15 -0500

    add details to coding challenge for 2022-10-26

---
+++ cc-2022-10-26/js/main.js
@@ -0,0 +1,15 @@
+// PREP
+
+// Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+// spacify("hello world") // returns "h e l l o   w o r l d"
+
+// P: a string
+// R: the given string with spaces inserted between each character
+// E: spacify("hello world") // returns "h e l l o   w o r l d"
+// P:
+
+// create a function that takes in a string
+function spacify(str) {
+
+}
