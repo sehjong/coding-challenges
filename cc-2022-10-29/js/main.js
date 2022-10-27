4b0b9c6d99c17757ee1e01d84649c975e1d2cca2
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-10-27 11:06:12 -0500

    add details to coding challenge for 2022-10-29

---
+++ cc-2022-10-29/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// Complete the method which accepts an array of integers, and returns one of the following:
+
+// "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+// You can assume the array will always be valid, and there will always be one correct answer.
+
+// P: an array of integers, the array will always be valid, and there will always be one correct answer
+// R: "yes, ascending" - if the numbers in the array are sorted in an ascending order
+//    "yes, descending" - if the numbers in the array are sorted in a descending order
+//    "no" - otherwise
+// E: console.log(isSortedAndHow([1, 2]), 'yes, ascending');
+//    console.log(isSortedAndHow([15, 7, 3, -8]), 'yes, descending');
+//    console.log(isSortedAndHow([4, 2, 30]), 'no');
+// P:
+
+// create a function that takes in an array of numbers
+function isSortedAndHow(arr) {
+
+}
