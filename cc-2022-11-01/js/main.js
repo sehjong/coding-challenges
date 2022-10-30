dc29981725e038386c44f0e8cfaa5101f4c772b1
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-10-30 17:09:24 -0500

    add details to coding challenge for 2022-11-01

---
+++ cc-2022-11-01/js/main.js
@@ -0,0 +1,21 @@
+// PREP
+
+// Write a function that finds the sum of all its arguments.
+
+// eg:
+
+// sum(1, 2, 3) // => 6
+// sum(8, 2) // => 10
+// sum(1, 2, 3, 4, 5) // => 15
+
+// P: integers only
+// R: the sum of all its arguments
+// E: sum(1, 2, 3) // => 6
+//    sum(8, 2) // => 10
+//    sum(1, 2, 3, 4, 5) // => 15
+// P:
+
+// create a function that takes in numbers
+function sum() {
+
+}
