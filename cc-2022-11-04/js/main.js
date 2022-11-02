5f59f036f38a58e48fe863dd5b2ced5ce3dd06f3
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-11-02 13:15:25 -0500

    add details to coding challenge for 2022-11-04

---
+++ cc-2022-11-04/js/main.js
@@ -0,0 +1,16 @@
+// PREP
+
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+// P: a string of word(s)
+// R: the length of the shortest word(s)
+// E: console.log(findShort("lets talk about javascript the best language"), 3);
+//    console.log(findShort("i want to travel the world writing code one day"), 1);
+// P:
+
+// create a function that takes in an string of word(s)
+function findShort(str) {
+
+}
