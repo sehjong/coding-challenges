3c0859bb9a70dbe895378e719f4f1cb850244c97
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-11-05 13:42:45 -0500

    add details to coding challenge for 2022-11-07

---
+++ cc-2022-11-07/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+// P: an array of integers
+// R: that single different number
+// E: [1, 1, 2] ==> 2
+//    [17, 17, 3, 17, 17, 17, 17] ==> 3
+// P:
+
+// create a function that takes in an array of numbers
+function stray(nums) {
+
+}
