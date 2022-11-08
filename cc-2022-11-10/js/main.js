001c0d5f2c96e7d79f13d654da07204a778564af
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-11-08 13:38:17 -0600

    add details to coding challenge for 2022-11-10

---
+++ cc-2022-11-10/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+// Find the number with the most digits.
+
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+// P: an array of numbers
+// R: the number with the most digits
+// E: console.log(findLongest([1, 10, 100]), 100);
+//    console.log(findLongest([9000, 8, 800]), 9000);
+//    console.log(findLongest([1, 200, 100000]), 100000);
+// P:
+
+// create a function that takes in an array of numbers
+function findLongest(intArr) {
+
+}
