427527e12fa5612005a6da511ccc3e90307c66c8
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-11-11 12:02:20 -0600

    add details to coding challenge for 2022-11-13

---
+++ cc-2022-11-13/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// Filter the number
+// Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+
+// Task
+// Your task is to return a number from a string.
+
+// Details
+// You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+
+// P: you will be given a string of numbers and letters mixed up
+// R: all the numbers in that string in the order they occur
+// E: console.log(filterString("123"), 123);
+//    console.log(filterString("a1b2c3"), 123);
+//    console.log(filterString("aa1bb2cc3dd"), 123);
+// P:
+
+// create a function expression that takes in a string of numbers and letters
+let filterString = function(value) {
+
+}
