a3f69e60171102cabde94599e615569a3c8bbca8
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-11-14 09:56:18 -0600

    add details to coding challenge for 2022-11-16

---
+++ cc-2022-11-16/js/main.js
@@ -0,0 +1,13 @@
+// PREP
+
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// P: a single string (word)
+// R: an ordered list containing the indexes of all capital letters in the string
+// E: console.log(capitals('hElLo'), [1, 3]);
+// P:
+
+// create a function expression that takes in a single string (word)
+let capitals = function(word) {
+    
+}
