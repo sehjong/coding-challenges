6a4f9ece38955e815d5c06cd81b681ea51bf82c7
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-11-17 10:51:02 -0600

    add details to coding challenge for 2022-11-19

---
+++ cc-2022-11-19/js/main.js
@@ -0,0 +1,15 @@
+// PREP
+
+// Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+// All inputs will be valid.
+
+// P: integer
+// R: total number of digits in the integer
+// E: console.log(digits(101), 3);
+// P:
+
+// create a function that takes in a number
+function digits(num) {
+
+}
