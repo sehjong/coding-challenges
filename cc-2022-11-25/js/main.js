d8900d0e65256310cd7aaae96990b06638ebd559
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-11-24 17:03:48 -0600

    add attempt to coding challenge for 2022-11-25

--- cc-2022-11-25/js/main.js
+++ cc-2022-11-25/js/main.js
@@ -19,5 +19,7 @@
 
 // create a function that takes in a string
 function solve(str) {
-
+    let upper = str.split(' ').filter(x => x === x.toUpperCase()).length;
+    let lower = str.length - upper;
+    return (upper > lower) ? str.toUpperCase() : str.toLowerCase();
 }
