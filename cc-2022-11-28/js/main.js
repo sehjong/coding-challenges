acf02a49207f76077e95763b6126a00be2440030
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-11-26 15:24:53 -0600

    add details to coding challenge for 2022-11-28

---
+++ cc-2022-11-28/js/main.js
@@ -0,0 +1,26 @@
+// PREP
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+// P: strings s1 and s2, letters only
+// R: a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2
+// E: a = "xyaabbbccccdefww"
+//    b = "xxxxyyyyabklmopq"
+//    longest(a, b) -> "abcdefklmopqwxy"
+
+//    a = "abcdefghijklmnopqrstuvwxyz"
+//    longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+// P:
+
+// create a function that takes in strings
+function longest(s1, s2) {
+    
+}
