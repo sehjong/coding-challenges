398d3d3b0962cc3d2a8fbce52353234d64ad396e
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-12-02 15:52:26 -0600

    add details to coding challenge for 2022-12-04

---
+++ cc-2022-12-04/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+// P: strings
+// R: true if the first argument(string) passed in ends with the 2nd argument (also a string)
+// E: solution('abc', 'bc') // returns true
+//    solution('abc', 'd') // returns false
+// P:
+
+// create a function that takes in strings
+function solution(str, ending) {
+    
+}
