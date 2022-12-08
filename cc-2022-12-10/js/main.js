6ab45fa64427593adb698c59394a45b8eb74e5d7
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-12-08 05:41:41 -0600

    add details to coding challenge for 2022-12-10

---
+++ cc-2022-12-10/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+// Example:
+
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+
+// P: string of letters
+// R: switch the position of letters a and b
+// E: 'acb' --> 'bca'
+//    'aabacbaa' --> 'bbabcabb'
+// P:
+
+// create a function that takes in a string of letters
+function switcheroo() {
+    
+}
