32dc03d179f9861b7212d886a232e537fec79769
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-12-11 07:07:54 -0600

    add details to coding challenge for 2022-12-13

---
+++ cc-2022-12-13/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// Oh no!
+// Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
+
+// You need to cast the whole array to the correct type.
+
+// Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+
+// ie:["1", "2", "3"] to [1, 2, 3]
+
+// Note that you can receive floats as well.
+
+// P: integers as strings, floats
+// R: sequence of numbers represented as strings and outputs a sequence of numbers
+// E: ["1", "2", "3"] to [1, 2, 3]
+// P:
+
+// create a function that takes in strings of numbers
+function toNumberArray(strArr) {
+
+}
