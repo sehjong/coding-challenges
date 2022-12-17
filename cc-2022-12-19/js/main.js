1b40169101f3fea83f59e3dda029bc86f5a3113c
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-12-17 12:01:47 -0600

    add details to coding challenge for 2022-12-19

---
+++ cc-2022-12-19/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// Write a function that returns only the decimal part of the given number.
+
+// You only have to handle valid numbers, not Infinity, NaN, or similar. Always return a positive decimal part.
+
+// Examples
+// getDecimal(2.4)  === 0.4
+// getDecimal(-0.2) === 0.2
+
+// P: integer, float, no infinity, no NaN, etc.
+// R: only the (positive) decimal part of the given number
+// E: getDecimal(2.4)  === 0.4
+//    getDecimal(-0.2) === 0.2
+// P:
+
+// create a function that takes in an integer
+function getDecimal(num) {
+
+}
