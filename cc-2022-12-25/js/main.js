2c929896f88d8a80ca1eb475eee8443e5a09bddb
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-12-23 22:46:12 -0600

    add details to coding challenge for 2022-12-25

---
+++ cc-2022-12-25/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// Complete the function to find the count of the most frequent item of an array. You can assume that input is an array of integers. For an empty array return 0
+
+// Example
+// input array: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]
+// ouptut: 5 
+// The most frequent number in the array is -1 and it occurs 5 times.
+
+// P: an array of integers
+// R: the count of the most frequent item of an array, for an empty array return 0
+// E: input array: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]
+//    ouptut: 5 
+//    The most frequent number in the array is -1 and it occurs 5 times.
+// P:
+
+// create a function that takes in an array of integers
+function mostFrequentItemCount(collection) {
+    
+}
