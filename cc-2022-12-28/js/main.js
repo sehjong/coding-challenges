4f764b455cf207f0a44976f1b1f2be858a59942e
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-12-26 21:38:56 -0600

    add details to coding challenge for 2022-12-28

---
+++ cc-2022-12-28/js/main.js
@@ -0,0 +1,28 @@
+// PREP
+
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+// P: an array of integers
+// R: when provided with a triplet, returns the index of the numerical element that lies between the other two elements
+// E: gimme([2, 3, 1]) => 0
+//    2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+//    gimme([5, 10, 14]) => 1
+//    10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+// P:
+
+// create a function that takes in an array of numbers
+function gimme(intArr) {
+
+}
