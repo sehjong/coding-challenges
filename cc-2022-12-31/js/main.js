19bee2811825a60e9ac3c1ce0f5a0dc8008bad9b
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2022-12-31 10:31:22 -0600

    add solution to coding challenge for 2022-12-31

--- cc-2022-12-31/js/main.js
+++ cc-2022-12-31/js/main.js
@@ -31,4 +31,6 @@ function lastSurvivor(letters, coords) {
     for(let i = 0; i < coords.length; i++) {
         letters.splice(coords[i], 1);
     }
+    return letters.join('');
 }
+console.log(lastSurvivor('zbk', [0, 1]), 'b');
