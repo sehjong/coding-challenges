acbafc63015b387a71eebce6cd2d066ddcca9491
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-01-01 14:16:39 -0600

    add details to coding challenge for 2023-01-03

---
+++ cc-2023-01-03/js/main.js
@@ -0,0 +1,23 @@
+// PREP
+
+// In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+
+// years divisible by 4 are leap years
+// but years divisible by 100 are not leap years
+// but years divisible by 400 are leap years
+// Additional Notes:
+
+// Only valid years (positive integers) will be tested, so you don't have to validate them
+// Examples can be found in the test fixture.
+
+// P: whole numbers only
+// R: whether a given year is a leap year or not
+// E: console.log(isLeapYear(2004), true);
+//    console.log(isLeapYear(1234), false);
+//    console.log(isLeapYear(2010), false);
+// P:
+
+// create a function that takes in a given year in whole numbers
+function isLeapYear(year) {
+
+}
