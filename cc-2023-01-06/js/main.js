8641b6e1b3fa0b7943ea520a5503354a97ca5527
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-01-04 19:09:48 -0600

    add details to coding challenge for 2023-01-06

---
+++ cc-2023-01-06/js/main.js
@@ -0,0 +1,21 @@
+// PREP
+
+// Given a string of digits confirm whether the sum of all the individual even digits are greater than the sum of all the indiviudal odd digits. Always a string of numbers will be given.
+
+// If the sum of even numbers is greater than the odd numbers return: "Even is greater than Odd"
+
+// If the sum of odd numbers is greater than the sum of even numbers return: "Odd is greater than Even"
+
+// If the total of both even and odd numbers are identical return: "Even and Odd are the same"
+
+// P: a string of numbers
+// R: confirm whether the sum of all the individual even digits are greater than the sum of all the indiviudal odd digits
+// E: console.log(evenOrOdd('1234'), 'Even is greater than Odd');
+//    console.log(evenOrOdd('123'), 'Odd is greater than Even');
+//    console.log(evenOrOdd('112'), 'Even and Odd are the same');
+// P:
+
+// create a function that takes in a string of numbers
+function evenOrOdd(strNums) {
+
+}
