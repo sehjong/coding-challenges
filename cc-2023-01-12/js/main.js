053a4c6e6f0d4a9eadb83cddf7cbebfea4856ed9
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-01-10 11:13:09 -0600

    add details to coding challenge for 2023-01-12

---
+++ cc-2023-01-12/js/main.js
@@ -0,0 +1,24 @@
+// PREP
+
+// Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+// Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+
+// var addOne = add(1);
+// addOne(3); // 4
+
+// var addThree = add(3);
+// addThree(3); // 6
+
+// P: integer
+// R: a function that always adds n to any number
+// E: var addOne = add(1);
+//    addOne(3); // 4
+//    var addThree = add(3);
+//    addThree(3); // 6
+// P:
+
+// create a function that takes in an integer
+function add(n) {
+
+}
