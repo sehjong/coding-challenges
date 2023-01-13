25a05323bfc8e9ed994a01e938b365ea576c2938
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-01-13 12:51:17 -0600

    add details to coding challenge for 2023-01-15

---
+++ cc-2023-01-15/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Given a positive integer n, calculate the following sum:
+
+// n + n/2 + n/4 + n/8 + ...
+// All elements of the sum are the results of integer division.
+
+// Example
+// 25  =>  25 + 12 + 6 + 3 + 1 = 47
+
+// P: positive integer
+// R: the sum, all elements of the sum are the results of integer division
+// E: 25  =>  25 + 12 + 6 + 3 + 1 = 47
+// P:
+
+//create a function that takes in a number
+function halvingSum(num) {
+    
+}
