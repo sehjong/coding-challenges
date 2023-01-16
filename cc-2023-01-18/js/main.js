d67415f7c8afed0f0aab6092d7eb7cebf9c80472
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-01-16 13:27:57 -0600

    add details to coding challenge for 2023-01-18

---
+++ cc-2023-01-18/js/main.js
@@ -0,0 +1,23 @@
+// PREP
+
+// Given a non-negative integer, return an array / a list of the individual digits in order.
+
+// Examples:
+
+// 123 => [1,2,3]
+
+// 1 => [1]
+
+// 8675309 => [8,6,7,5,3,0,9]
+
+// P: whole number
+// R: an array / a list of the individual digits in order
+// E: 123 => [1,2,3]
+//    1 => [1]
+//    8675309 => [8,6,7,5,3,0,9]
+// P:
+
+// create a function that takes in a whole number
+function digitize(num) {
+    
+}
