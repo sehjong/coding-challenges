21b4a20be9afd5ec3a11325e1c5f4019b6079f3e
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-01-19 16:44:22 -0600

    add details to coding challenge for 2023-01-21

---
+++ cc-2023-01-21/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// Given a varying number of integer arguments, return the digits that are not present in any of them.
+
+// Example:
+
+// [12, 34, 56, 78]  =>  "09"
+// [2015, 8, 26]     =>  "3479"
+// Note: the digits in the resulting string should be sorted.
+
+// P: whole numbers
+// R: the digits that are not present in any of them
+// E: [12, 34, 56, 78]  =>  "09"
+//    [2015, 8, 26]     =>  "3479"
+// P:
+
+// create a function that takes in whole numbers
+function unusedDigits() {
+
+}
