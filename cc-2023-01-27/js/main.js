6df41feff1b234264b48d76318a7611d5d5afe01
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-01-25 22:59:20 -0600

    add details to coding challenge for 2023-01-27

---
+++ cc-2023-01-27/js/main.js
@@ -0,0 +1,16 @@
+// PREP
+
+// Write the definition of the function "say" such that calling this:
+
+// say("Hello")("World")
+// returns "Hello World"
+
+// P: string
+// R: "Hello World"
+// E: console.log(say('Hello')('World'), 'Hello World');
+// P:
+
+// create a function expression that takes in a string
+let say = function() {
+    
+}
