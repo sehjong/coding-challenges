82f8d40939fdf8ffd67f6cc9f67c86e599020bfd
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-02-08 11:19:42 -0600

    add solution to coding challenge for 2023-02-08

--- cc-2023-02-08/js/main.js
+++ cc-2023-02-08/js/main.js
@@ -18,8 +18,13 @@
 function divisors(integer) {
     let arr = [];
     for(let i = 2; i < integer; i++) {
-        if(integer % 1 == 0) {
+        if(integer % i == 0) {
             arr.push(i);
         }
+    }
+    if(arr.length == 0) {
+        return `${integer} is prime`;
+    }else{
+        return arr;
     }
 }
