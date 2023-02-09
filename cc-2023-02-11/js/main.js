6077cf642c5b713e15cdaf1f2106ed6d8dfacdcd
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-02-09 16:49:25 -0600

    add details to coding challenge for 2023-02-11

---
+++ cc-2023-02-11/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+// P: string of lowercase latin letters, digits and symbols
+// R: the reverse of that string with all non-alphabetic characters omitted
+// E: For str = "krishan", the output should be "nahsirk"
+//    For str = "ultr53o?n", the output should be "nortlu"
+// P:
+
+// create a function that takes in a string of lowercase latin letters, digits and symbols
+function reverseLetter(str) {
+
+}
