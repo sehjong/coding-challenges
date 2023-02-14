6e1b64610f66665378fbfba2707ad36d8a8cd9a7
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-02-14 13:16:48 -0600

    add solution to coding challenge for 2023-02-14

--- cc-2023-02-14/js/main.js
+++ cc-2023-02-14/js/main.js
@@ -29,5 +29,5 @@
 const getSum = (a, b) => {
     let min = Math.min(a, b),
         max = Math.max(a, b);
-        (max - min + 1) * (min + max) / 2;
+    return (max - min + 1) * (min + max) / 2;
 }
