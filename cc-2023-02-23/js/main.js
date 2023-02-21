0d8d7fc9d63759820d96a6136023c3e37d891c37
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-02-21 16:21:06 -0600

    add details to coding challenge for 2023-02-23

---
+++ cc-2023-02-23/js/main.js
@@ -0,0 +1,23 @@
+// PREP
+
+// You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+// lst contains integers, that means it may contain some negative numbers
+// if lst is empty or contains a single element, return 0
+// lst is not sorted
+// [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+// [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+// Have fun!
+
+// P: an array of integers
+// R: the difference between the largest and the smallest value in a given list / array (lst) received as the parameter
+//    if lst is empty or contains a single element, return 0
+//    lst is not sorted
+// E: [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+//    [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+// P:
+
+// create a function that takes in an array of integers
+function maxDiff(list) {
+
+}
