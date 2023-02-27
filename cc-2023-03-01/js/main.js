b0871ec32af797d9a4cb85acc1abe8f5313fbf57
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-02-27 11:51:12 -0600

    add details to coding challenge for 2023-03-01

---
+++ cc-2023-03-01/js/main.js
@@ -0,0 +1,23 @@
+// PREP
+
+// Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+
+// All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+
+// Examples:
+
+// vowelOne( "abceios" ) // "1001110"
+
+// vowelOne( "aeiou, abc" ) // "1111100100"
+
+// P: string
+// R: a strings of 1's and 0's where vowels become 1's and non-vowels become 0's
+//    all non-vowels including non alpha characters (spaces,commas etc.) should be included
+// E: vowelOne( "abceios" ) // "1001110"
+//    vowelOne( "aeiou, abc" ) // "1111100100"
+// P:
+
+// create a function that takes in a string
+function vowelOne(str) {
+
+}
