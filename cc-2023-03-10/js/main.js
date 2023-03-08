4a0da6ae24cb0ba437679a18f9c341eb490df1b0
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-03-08 14:54:38 -0600

    add details to coding challenge for 2023-03-10

---
+++ cc-2023-03-10/js/main.js
@@ -0,0 +1,21 @@
+// PREP
+
+// Write a function that accepts two arguments: an array/list of integers and another integer (n).
+
+// Determine the number of times where two integers in the array have a difference of n.
+
+// For example:
+
+// [1, 1, 5, 6, 9, 16, 27], n=4  -->  3  # (1,5), (1,5), (5,9)
+// [1, 1, 3, 3], n=2             -->  4  # (1,3), (1,3), (1,3), (1,3)
+
+// P: an array/list of integers and another integer (n)
+// R: the number of times where two integers in the array have a difference of n
+// E: [1, 1, 5, 6, 9, 16, 27], n=4  -->  3  # (1,5), (1,5), (5,9)
+//    [1, 1, 3, 3], n=2             -->  4  # (1,3), (1,3), (1,3), (1,3)
+// P:
+
+// create a function expression that takes in an array of integers and another integer
+const intDiff = (intArr, int) => {
+
+}
