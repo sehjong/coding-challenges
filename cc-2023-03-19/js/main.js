4073db934c5fc2408818761f70268af4e832e6ce
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-03-17 18:17:30 -0500

    add details to coding challenge for 2023-03-19

---
+++ cc-2023-03-19/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// Write a program that outputs the top n elements from a list.
+
+// Example:
+
+// largest(2, [7,6,5,4,3,2,1])
+// => [6,7]
+
+// P: an array of numbers
+// R: the top n elements from a list
+// E: largest(2, [7,6,5,4,3,2,1])
+//    => [6,7]
+// P:
+
+// create a function that takes in two parameters as arguments,
+// the number of elements to output and an array of numbers to output from
+function largest(n, arrNums) {
+
+}
