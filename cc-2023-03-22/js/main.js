49a80bad3a2536999b69bd451c3a1707715ec6a4
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-03-20 19:54:15 -0500

    add details to coding challenge for 2023-03-22

---
+++ cc-2023-03-22/js/main.js
@@ -0,0 +1,33 @@
+// PREP
+
+// Complete the function which takes a non-zero integer as its argument.
+
+// If the integer is divisible by 3, return the string "Java".
+
+// If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+
+// If one of the condition above is true and the integer is even, add "Script" to the end of the string.
+
+// If none of the condition is true, return the string "mocha_missing!"
+
+// Examples
+// 1   -->  "mocha_missing!"
+// 3   -->  "Java"
+// 6   -->  "JavaScript"
+// 12  -->  "CoffeeScript"
+
+// P: a non-zero integer
+// R: If the integer is divisible by 3, return the string "Java".
+//    If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+//    If one of the condition above is true and the integer is even, add "Script" to the end of the string.
+//    If none of the condition is true, return the string "mocha_missing!"
+// E: 1   -->  "mocha_missing!"
+//    3   -->  "Java"
+//    6   -->  "JavaScript"
+//    12  -->  "CoffeeScript"
+// P:
+
+// create a function that takes in a non-zero integer
+function caffeineBuzz(num) {
+
+}
