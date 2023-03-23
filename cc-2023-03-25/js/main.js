ae4b3d4813743444ca7130657195bbd885a23211
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-03-23 13:11:02 -0500

    add details to coding challenge for 2023-03-25

---
+++ cc-2023-03-25/js/main.js
@@ -0,0 +1,21 @@
+// PREP
+
+// Find the last element of the given argument(s).
+
+// Examples
+// last([1, 2, 3, 4] ) // =>  4
+// last("xyz")         // => "z"
+// last(1, 2, 3, 4)    // =>  4
+// In javascript and CoffeeScript a list will be an array, a string or the list of arguments.
+
+// P: numbers and strings
+// R: the last element of the given argument(s)
+// E: last([1, 2, 3, 4] ) // =>  4
+//    last("xyz")         // => "z"
+//    last(1, 2, 3, 4)    // =>  4
+// P:
+
+// create a function that takes in numbers and strings
+function last(list) {
+
+}
