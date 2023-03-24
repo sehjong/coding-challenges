617db206e593a765de85057955333e3fca907743
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-03-24 13:24:46 -0500

    add attempt to coding challenge for 2023-03-25

--- cc-2023-03-25/js/main.js
+++ cc-2023-03-25/js/main.js
@@ -17,5 +17,14 @@
 
 // create a function that takes in numbers and strings
 function last(list) {
-
+    if(arguments.length - 1) {
+        return arguments[arguments.length - 1];
+    }
+    if(Array.isArray(list)) {
+        return list.pop();
+    }else if(typeof list === 'string') {
+        return list.charAt(list.length -1);
+    }else{
+        return list;
+    }
 }
