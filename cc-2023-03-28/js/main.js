086b4530680cc802c30e51c863d71bd3217bb119
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-03-26 14:04:48 -0500

    add details to coding challenge for 2023-03-28

---
+++ cc-2023-03-28/js/main.js
@@ -0,0 +1,18 @@
+// PREP
+
+// Write a function insert_dash(num) / insertDash(num) / InsertDash(int num) that will insert dashes ('-') between each two odd digits in num. For example: if num is 454793 the output should be 4547-9-3. Don't count zero as an odd digit.
+
+// Note that the number will always be non-negative (>= 0).
+
+// P: whole numbers
+// R: dashes ('-') between each two odd digits,
+//    don't count zero as an odd digit
+// E: console.log(insertDash(454793), '4547-9-3');
+//    console.log(insertDash(123456), '123456');
+//    console.log(insertDash(1003567), '1003-567');
+// P:
+
+// create a function that takes in whole numbers
+function insertDash(num) {
+
+}
