8483097305bff6221019066b482298d71ab54fb7
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-03-27 14:15:43 -0500

    add attempt to coding challenge for 2023-03-28

--- cc-2023-03-28/js/main.js
+++ cc-2023-03-28/js/main.js
@@ -14,5 +14,11 @@
 
 // create a function that takes in whole numbers
 function insertDash(num) {
-
+    num = num.toString().split(' ');
+    for(let i = 0; i < num.length; i++) {
+        if(num[i] % 2 > 0 && num[i + 1] % 2 > 0) {
+            num[i] = num[i] + '-';
+        }
+    }
+    return num.join('');
 }
