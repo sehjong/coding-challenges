7d7422a2d854a4ba26f01b61e1330aa38966d893
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-03-28 14:17:29 -0500

    add solution to coding challenge for 2023-03-28

--- cc-2023-03-28/js/main.js
+++ cc-2023-03-28/js/main.js
@@ -14,7 +14,7 @@
 
 // create a function that takes in whole numbers
 function insertDash(num) {
-    num = num.toString().split(' ');
+    num = num.toString().split('');
     for(let i = 0; i < num.length; i++) {
         if(num[i] % 2 > 0 && num[i + 1] % 2 > 0) {
             num[i] = num[i] + '-';
