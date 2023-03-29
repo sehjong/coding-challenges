3a63ae865c3ba9d9f998195f1821ee874b7ad91c
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-03-29 14:54:56 -0500

    add details to coding challenge for 2023-03-31

---
+++ cc-2023-03-31/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+// For example:
+
+// capitalize("abcdef",[1,2,5]) = "aBCdeF"
+// capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+// The input will be a lowercase string with no spaces and an array of digits.
+
+// P: a string and an array of integers representing indices
+//    the string will be a lowercase string with no spaces and
+//    the array will be an array of digits
+// R: capitalize all letters at the given indices
+// E: capitalize("abcdef",[1,2,5]) = "aBCdeF"
+//    capitalize("abcdef",[1,2,5,100]) = "aBCdeF" - There is no index 100
+// P:
+
+// create a function that takes in a string and an array of integers representing indices
+function capitalize(str, intArr) {
+
+}
