0ba6eeedabf59b60f3f64674723f59074e9e33dd
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-04-03 18:21:36 -0500

    add solution to coding challenge for 2023-04-03

--- cc-2023-04-03/js/main.js
+++ cc-2023-04-03/js/main.js
@@ -22,6 +22,6 @@ const findMissing = (arr1, arr2) => {
     arr1 = arr1.sort();
     arr2 = arr2.sort();
     for(let i = 0; i < arr1.length; i++) {
-        if(arr1[i] != arr2[i]) arr1[i];
+        if(arr1[i] != arr2[i]) return arr1[i];
     }
 }
