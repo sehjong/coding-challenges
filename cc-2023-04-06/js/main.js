5df064792a4e0ab08f64203af3fec641fcbdb18e
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-04-04 18:51:52 -0500

    add details to coding challenge for 2023-04-06

---
+++ cc-2023-04-06/js/main.js
@@ -0,0 +1,21 @@
+// PREP
+
+// When provided with a String, capitalize all vowels
+
+// For example:
+
+// Input : "Hello World!"
+
+// Output : "HEllO WOrld!"
+
+// Note: Y is not a vowel in this kata.
+
+// P: a string of word(s)
+// R: the string of word(s) with vowels capitalized
+//    Y is not a vowel for this kata
+// E: Input : "Hello World!"
+//    Output : "HEllO WOrld!"
+// P:
+
+// create a function expression that takes in an array of vowel letters as strings
+const vowels = ['a', 'e', 'i', 'o', 'u'];
