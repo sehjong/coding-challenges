722ff7eb7cf99468da0db8618ba74cb28c34a231
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-04-13 12:09:46 -0500

    add details to coding challenge for 2023-04-15

---
+++ cc-2023-04-15/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// In this Kata, you will be given two strings a and b and your task will be to return the characters that are not common in the two strings.
+
+// For example:
+
+// solve("xyab","xzca") = "ybzc" 
+// --The first string has 'yb' which is not in the second string. 
+// --The second string has 'zc' which is not in the first string. 
+// Notice also that you return the characters from the first string concatenated with those from the second string.
+
+// P: two strings of letters
+// R: the characters that are not common in the two strings,
+//    with the characters from the first string concatenated
+//    with those from the second string
+// E: solve("xyab","xzca") = "ybzc"
+// P:
+
+// create a function that takes in two strings of letters
+function solve(a, b) {
+    
+}
