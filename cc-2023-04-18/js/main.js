fbf1b28d0f2e0bae376fb7433f121e60d3c1f831
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-04-16 13:38:07 -0500

    add details to coding challenge for 2023-04-18

---
+++ cc-2023-04-18/js/main.js
@@ -0,0 +1,27 @@
+// PREP
+
+// Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+// Example:
+
+// Input:
+
+// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+// Output:
+
+// 'alpha beta gamma delta'
+
+// P: a string of words
+// R: output a string with all duplicate words from the input string removed,
+//    leaving only single (first) words entries
+// E: Input:
+//    'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+//    Output:
+//    'alpha beta gamma delta'
+// P:
+
+// create a function expression that takes in a string of words
+const removeDuplicateWords = str => {
+    
+}
