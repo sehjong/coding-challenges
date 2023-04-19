63fe28aff05b5703de80cd335f7c4bd67e8815fe
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-04-19 15:12:46 -0500

    add details to coding challenge for 2023-04-21

---
+++ cc-2023-04-21/js/main.js
@@ -0,0 +1,18 @@
+// PREP
+
+// Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+
+// Return as a number.
+
+// P: a mixed array of number and string representations of integers
+// R: add up the non-string integers and subtract the total of the string integers
+//    and return as a number
+// E: console.log(sumNums([9, 3, '7', '3']), 2);
+//    console.log(sumNums(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 14);
+//    console.log(sumNums(['1', '5', '8', 8, 9, 9, 2, '3']), 11);
+// P:
+
+// create a function that takes in a mixed array of number and string representations of integers
+function sumNums(nums) {
+    
+}
