d0f3b561aa6e1eca4fe99820277e4ef09a206869
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-04-22 16:20:27 -0500

    add details to coding challenge for 2023-04-24

---
+++ cc-2023-04-24/js/main.js
@@ -0,0 +1,21 @@
+// PREP
+
+// Write a function that returns a sequence (index begins with 1) of all the even characters from a string. If the string is smaller than two characters or longer than 100 characters, the function should return "invalid string".
+
+// For example:
+
+// "abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
+// "a"             --> "invalid string"
+
+// P: an array of strings
+// R: all the even characters from a string from the array, index begins with 1
+//    if the string is smaller than two characters or longer than 100 characters,
+//    the function should return "invalid string"
+// E: "abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
+//    "a"             --> "invalid string"
+// P:
+
+// create a function that takes in an array of strings
+function evenChars(strArr) {
+
+}
