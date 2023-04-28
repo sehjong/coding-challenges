d003ec9e7f4855676b7954f8a1783d4e78e00161
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-04-28 13:19:11 -0500

    add details to coding challenge for 2023-04-30

---
+++ cc-2023-04-30/js/main.js
@@ -0,0 +1,31 @@
+// PREP
+
+// Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+
+// For Example:
+
+// [ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+// , [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+// , [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+// ]
+// So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+// Note: You will always be given a non-empty list containing positive values.
+
+// P: numbers in a matrix 2D array
+//    will always be a non-empty list containing positive values
+// R: the sum of the minimum values in each row
+// E: For Example:
+
+//    [ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+//    , [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+//    , [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+//    ]
+//    So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+// P:
+
+// create a function that takes in numbers in a matrix 2D array
+function sumOfMinimums(arr) {
+    
+}
