26553c795686a6e17251470d3fe92856171378f5
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-05-04 10:38:04 -0500

    add details to coding challenge for 2023-05-06

---
+++ cc-2023-05-06/js/main.js
@@ -0,0 +1,24 @@
+// PREP
+
+// Given an array of numbers, return the difference between the largest and smallest values.
+
+// For example:
+
+// [23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
+
+// [1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
+
+// The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
+
+// P: an array of numbers,
+//    the array will contain a minimum of two elements,
+//    input data range guarantees that max-min will cause no integer overflow
+// R: the difference between the largest and smallest values
+// E: [23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3)
+//    [1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1)
+// P:
+
+// create a function that takes in an array of numbers
+function minMax(arrNums) {
+    
+}
