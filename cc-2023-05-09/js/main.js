8116c3724db182709b083b47fad0a760026d62c6
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-05-07 13:08:57 -0500

    add details to coding challenge for 2023-05-09

---
+++ cc-2023-05-09/js/main.js
@@ -0,0 +1,23 @@
+// PREP
+
+// Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+// --> "alpha beta gamma delta alpha beta gamma delta"
+// Words will be separated by a single space. There will be no leading or trailing spaces in the string. An empty string (0 words) is a valid input.
+
+// P: a string of words,
+//    words will be separated by a single space,
+//    there will be no leading or trailing spaces in the string,
+//    an empty string (0 words) is a valid input
+// R: output a string with all consecutive duplicate words from the input string removed,
+//    leaving only first words entries
+// E: "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+// --> "alpha beta gamma delta alpha beta gamma delta"
+// P: 
+
+// create a function expression that takes in a string of words
+const removeConsecutiveDuplicateWords = str => {
+    
+}
