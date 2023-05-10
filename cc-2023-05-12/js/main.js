5db4025fc311577901e2757a0c447a0986db36fc
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-05-10 14:55:14 -0500

    add details to coding challenge for 2023-05-12

---
+++ cc-2023-05-12/js/main.js
@@ -0,0 +1,26 @@
+// PREP
+
+// Write a function named numbers.
+
+// function should return True if all parameters are of the Number type.
+
+// The function should accept any number of parameters.
+
+// Example usage:
+
+// numbers(1, 4, 3, 2, 5); // true
+// numbers(1, "a", 3); // false
+// numbers(1, 3, NaN); // true
+
+// P: numbers, strings,
+//    function should accept any number of parameters
+// R: True if all parameters are of the Number type
+// E: numbers(1, 4, 3, 2, 5); // true
+//    numbers(1, "a", 3); // false
+//    numbers(1, 3, NaN); // true
+// P:
+
+// create a function that takes in numbers and strings
+function numbers() {
+    
+}
