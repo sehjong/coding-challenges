c4d73621ab5e9cd6d2ff108efa1368cd7dc00cb6
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-05-20 16:39:53 -0500

    add attempt to coding challenge for 2023-05-21

--- cc-2023-05-21/js/main.js
+++ cc-2023-05-21/js/main.js
@@ -21,5 +21,10 @@
 
 // create a function that takes in numbers and strings
 function duplicates(arr) {
-
+    let x = [];
+    let y = [];
+    arr.forEach((i) => {
+        if(x.includes(i) === false) return x.push(i);
+        if(x.includes(i) && y.includes(i) === false) return y.push(i);
+    });
 }
