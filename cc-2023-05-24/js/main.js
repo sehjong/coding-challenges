1764fe1fa0dae1e80b0168a0d0f858d9eadfe7cf
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-05-22 18:18:51 -0500

    add details to coding challenge for 2023-05-24

---
+++ cc-2023-05-24/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// Given an array with exactly 5 strings "a", "b" or "c" (chars in Java, characters in Fortran), check if the array contains three and two of the same values.
+
+// Examples
+// ["a", "a", "a", "b", "b"] ==> true  // 3x "a" and 2x "b"
+// ["a", "b", "c", "b", "c"] ==> false // 1x "a", 2x "b" and 2x "c"
+// ["a", "a", "a", "a", "a"] ==> false // 5x "a"
+
+// P: an array with exactly 5 strings "a", "b" or "c"
+// R: check if the array contains three and two of the same values
+// E: ["a", "a", "a", "b", "b"] ==> true  // 3x "a" and 2x "b"
+//    ["a", "b", "c", "b", "c"] ==> false // 1x "a", 2x "b" and 2x "c"
+//    ["a", "a", "a", "a", "a"] ==> false // 5x "a"
+// P:
+
+// create a function that takes in an array of 5 strings comprising of "a", "b" or "c"
+function checkThreeAndTwo(arr) {
+
+}
