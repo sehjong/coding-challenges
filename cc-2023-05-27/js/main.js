a16f5138485c2feb2d9403e70abdcfed9be6512a
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-05-25 10:15:57 -0500

    add details to coding challenge for 2023-05-27

---
+++ cc-2023-05-27/js/main.js
@@ -0,0 +1,24 @@
+// PREP
+
+// Say hello!
+
+// Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null/nil/None if input is empty string or null/nil/None.
+
+// Example:
+
+// greet("Niks") === "hello Niks!";
+// greet("") === null; // Return null if input is empty string
+// greet(null) === null; // Return null if input is null
+
+// P: string
+// R: append 'hello' to input string,
+//    null/nil/None if input is empty string or null/nil/None
+// E: greet("Niks") === "hello Niks!";
+//    greet("") === null; // Return null if input is empty string
+//    greet(null) === null; // Return null if input is null
+// P: 
+
+// create a function that takes in a string
+function greet(name) {
+
+}
