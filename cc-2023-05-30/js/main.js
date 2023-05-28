397a8c6c0dd3a34755bd5acdb2dcefefce37caa1
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-05-28 15:18:18 -0500

    add details to coding challenge for 2023-05-30

---
+++ cc-2023-05-30/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+// You are going to be given a word. Your job will be to make sure that each character in that word has the exact same number of occurrences. You will return true if it is valid, or false if it is not.
+
+// For this kata, capitals are considered the same as lowercase letters. Therefore: "A" == "a"
+
+// The input is a string (with no spaces) containing [a-z],[A-Z],[0-9] and common symbols. The length will be 0 < length < 100.
+
+// Examples
+// "abcabc" is a valid word because "a" appears twice, "b" appears twice, and"c" appears twice.
+// "abcabcd" is NOT a valid word because "a" appears twice, "b" appears twice, "c" appears twice, but "d" only appears once!
+// "123abc!" is a valid word because all of the characters only appear once in the word.
+
+// P: a string of words, numbers, and symbols, with no spaces, containing [a-z],[A-Z],[0-9],
+//    capitals are considered the same as lowercase letters, therefore: "A" == "a"
+// R: true if that string has the exact same number of occurrences, or false if it is not
+// E: "abcabc" is a valid word because "a" appears twice, "b" appears twice, and"c" appears twice.
+//    "abcabcd" is NOT a valid word because "a" appears twice, "b" appears twice, "c" appears twice, but "d" only appears once!
+//    "123abc!" is a valid word because all of the characters only appear once in the word.
+// P:
+
+// create a function that takes in a string of words, numbers, and symbols
+function validateWord(str) {
+
+}
