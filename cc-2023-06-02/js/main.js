d067f23478cbf9689d6a50e83acada02b8bc8e1f
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-05-31 11:46:28 -0500

    add details to coding challenge for 2023-06-02

---
+++ cc-2023-06-02/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+// Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+// Example:
+
+// solution(5) // should return "Value is 00005"
+
+// P: a number
+// R: a formatted string, the return value should equal "Value is VALUE" where value is a 5 digit padded number
+// E: solution(5) // should return "Value is 00005"
+// P:
+
+// create a function that takes in a number
+function solution(value) {
+
+}
