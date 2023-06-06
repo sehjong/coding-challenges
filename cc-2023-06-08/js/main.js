62b398403b6b8375b7ddf1245838de510d1a4c38
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-06-06 13:53:14 -0500

    add details to coding challenge for 2023-06-08

---
+++ cc-2023-06-08/js/main.js
@@ -0,0 +1,15 @@
+// PREP
+
+// Your task in this kata is to implement a function that calculates the sum of the integers inside a string. For example, in the string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
+
+// Note: only positive integers will be tested.
+
+// P: a string of letters and whole numbers
+// R: the sum of the integers inside a string
+// E: "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635
+// P:
+
+// create a function that takes in a string of letters and whole numbers
+function sumInt(str) {
+    
+}
