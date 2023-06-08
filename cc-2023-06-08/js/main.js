12428e060ccdefd8824f97ed13ebb9e6899a1196
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-06-08 14:09:38 -0500

    add solution to coding challenge for 2023-06-08

--- cc-2023-06-08/js/main.js
+++ cc-2023-06-08/js/main.js
@@ -14,7 +14,7 @@ function sumInt(str) {
     let num = [];
     let nums = '';
     for(let i = 0; i < str.length; i++) {
-        if(!NaN(Number(str[i]))) {
+        if(!isNaN(Number(str[i]))) {
             nums += str[i];
             if(isNaN(Number(str[i + 1]))) {
                 num.push(Number(nums));
