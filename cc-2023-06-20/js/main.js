a0821d3eefb9fa3a5b262fb05f531ae4877f2807
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-06-18 12:23:42 -0500

    add details to coding challenge for 2023-06-20

---
+++ cc-2023-06-20/js/main.js
@@ -0,0 +1,26 @@
+// PREP
+
+// Given a number return the closest number to it that is divisible by 10.
+
+// Example input:
+
+// 22
+// 25
+// 37
+// Expected output:
+
+// 20
+// 30
+// 40
+
+// P: a number
+// R: the closest number to the input number that is divisible by 10
+// E: 22 -> 20
+//    25 -> 30
+//    37 -> 40
+// P:
+
+// create a function that takes in a number
+function closestMultiple10(num) {
+    
+}
