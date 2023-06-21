383ece51394058fa137ba6a4a936df02be028cbc
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-06-21 13:34:51 -0500

    add details to coding challenge for 2023-06-23

---
+++ cc-2023-06-23/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// Given an integer n and two other values, build an array of size n filled with these two values alternating.
+
+// Examples
+// 5, true, false     -->  [true, false, true, false, true]
+// 10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
+// 0, "one", "two"    -->  []
+
+// P: an integer n and two other values
+// R: an array of size n filled with these two values alternating
+// E: 5, true, false     -->  [true, false, true, false, true]
+//    10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
+//    0, "one", "two"    -->  []
+// P:
+
+// create a function that takes in an integer n and two other values
+function alternate(n, firstValue, secondValue) {
+    
+}
