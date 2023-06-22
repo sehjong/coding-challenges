d5c80cf1e627e43ca25456836ff955675a5c88c7
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-06-22 13:38:05 -0500

    add attempt to coding challenge for 2023-06-23

--- cc-2023-06-23/js/main.js
+++ cc-2023-06-23/js/main.js
@@ -16,5 +16,8 @@
 
 // create a function that takes in an integer n and two other values
 function alternate(n, firstValue, secondValue) {
-    
+    const array = [];
+    for(let i = 0; i < n; i++) {
+        array.push(i % 2 === 0 ? firstValue : secondValue);
+    }
 }
