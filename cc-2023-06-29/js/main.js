7259311333d96a3c18987d289522da621bc36cdd
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-06-27 13:14:31 -0500

    add details to coding challenge for 2023-06-29

---
+++ cc-2023-06-29/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// Count the number of occurrences of each character and return it as a (list of tuples) in order of appearance. For empty output return (an empty list).
+
+// Consult the solution set-up for the exact data structure implementation depending on your language.
+
+// Example:
+
+// orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+
+// P: a string of letters and numbers
+// R: an array as a (list of tuples) in order of appearance
+// E: orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+// P:
+
+// create a function expression that returns an array as a (list of tuples) in order of appearance,
+// from a string of letters and numbers
+const orderedCount = str => {
+
+}
