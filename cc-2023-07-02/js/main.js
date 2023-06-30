981f1265bc0106667c0e7b84faa7fa0142de79bd
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-06-30 15:19:30 -0500

    add details to coding challenge for 2023-07-02

---
+++ cc-2023-07-02/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+// flatten([1,2,3]) // => [1,2,3]
+// flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+// flatten([[[1,2,3]]]) // => [[1,2,3]]
+
+// P: an array of arrays of numbers and strings
+// R: a flattened array
+// E: flatten([1,2,3]) // => [1,2,3]
+//    flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+//    flatten([[[1,2,3]]]) // => [[1,2,3]]
+// P:
+
+// create a function expression that takes in an array of arrays of numbers and strings
+const flatten = (array) => {
+    
+}
