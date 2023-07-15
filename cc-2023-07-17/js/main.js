5042a94097e7471b04b09d6e2932a3b91b6fd869
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-07-15 13:43:35 -0500

    add details to coding challenge for 2023-07-17

---
+++ cc-2023-07-17/js/main.js
@@ -0,0 +1,18 @@
+// PREP
+
+// In this Kata, you will be given a string and your task will be to return a list of ints detailing the count of uppercase letters, lowercase, numbers and special characters, as follows.
+
+// Solve("*'&ABCDabcde12345") = [4,5,5,3]. 
+// --the order is: uppercase letters, lowercase, numbers and special characters.
+
+// P: a string containing uppercase letters, lowercase, numbers and special characters
+// R: an array of integers that holds the count of uppercase letters, lowercase letters, numbers,
+//    and special characters in the input string
+// E: Solve("*'&ABCDabcde12345") = [4,5,5,3]
+//    --the order is: uppercase letters, lowercase, numbers and special characters
+// P:
+
+// create a function expression
+const solve = (str) => {
+
+}
