9274589bd55ab1bea67810b3280c63e51d9c258f
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-07-21 18:50:08 -0500

    add details to coding challenge for 2023-07-23

---
+++ cc-2023-07-23/js/main.js
@@ -0,0 +1,18 @@
+// PREP
+
+// Create the function consecutive(arr) that takes an array of integers and return the minimum number of integers needed to make the contents of arr consecutive from the lowest number to the highest number.
+
+// For example:
+// If arr contains [4, 8, 6] then the output should be 2 because two numbers need to be added to the array (5 and 7) to make it a consecutive array of numbers from 4 to 8. Numbers in arr will be unique.
+
+// P: an array of unique integers
+// R: the minimum number of integers needed to make the contents of arr consecutive,
+//    from the lowest number to the highest number
+// E: [4, 8, 6] -> 2 because two numbers need to be added to the array (5 and 7),
+//    to make it a consecutive array of numbers from 4 to 8
+// P:
+
+// create a function that takes in an array of unique integers
+function consecutive(arr) {
+
+}
