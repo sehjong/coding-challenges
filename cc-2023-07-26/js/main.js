f8b7a5320c38ebdb51e45cc8b9e669411bea106d
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-07-24 12:59:00 -0500

    add details to coding challenge for 2023-07-26

---
+++ cc-2023-07-26/js/main.js
@@ -0,0 +1,18 @@
+// PREP
+
+// Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
+
+// The array will never be empty.
+
+// P: an array of numbers, the array will never be empty
+// R: the average of an array of numbers ("scores"),
+//    rounded to the nearest whole number
+// E: console.log(average([5, 78, 52, 900, 1]), 207);
+//    console.log(average([5, 25, 50, 75]), 39);
+//    console.log(average([2]), 2);
+// P:
+
+// create a function that takes in an array of numbers
+function average(scores) {
+    
+}
