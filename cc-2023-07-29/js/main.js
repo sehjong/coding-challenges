04d7ab1c84e92afa63b931e74d145f3069eedcde
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-07-27 10:13:59 -0500

    add details to coding challenge for 2023-07-29

---
+++ cc-2023-07-29/js/main.js
@@ -0,0 +1,21 @@
+// PREP
+
+// You'll be given a list of two strings, and each will contain exactly one colon (":") in the middle (but not at beginning or end). The length of the strings, before and after the colon, are random.
+
+// Your job is to return a list of two strings (in the same order as the original list), but with the characters after each colon swapped.
+
+// Examples
+// ["abc:123", "cde:456"]  -->  ["abc:456", "cde:123"]
+// ["a:12345", "777:xyz"]  -->  ["a:xyz", "777:12345"]
+
+// P: an array of two strings
+// R: a list of two strings (in the same order as the original list),
+//    but with the characters after each colon swapped
+// E: ["abc:123", "cde:456"]  -->  ["abc:456", "cde:123"]
+//    ["a:12345", "777:xyz"]  -->  ["a:xyz", "777:12345"]
+// P:
+
+// create a function that takes in an array of two strings
+function swap(arr) {
+
+}
