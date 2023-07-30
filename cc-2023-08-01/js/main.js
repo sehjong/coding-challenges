54460fd7b58e429dad7fc39aaed826a2d789cb64
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-07-30 10:20:23 -0500

    add details to coding challenge for 2023-08-01

---
+++ cc-2023-08-01/js/main.js
@@ -0,0 +1,30 @@
+// PREP
+
+// Create a function mispelled(word1, word2):
+
+// mispelled('versed', 'xersed'); // returns true
+// mispelled('versed', 'applb'); // returns false
+// mispelled('versed', 'v5rsed'); // returns true
+// mispelled('1versed', 'versed'); // returns true
+// mispelled('versed', 'versed'); // returns true
+// It checks if the word2 differs from word1 by at most one character.
+
+// This can include an extra char at the end or the beginning of either of words.
+
+// In the tests that expect true, the mispelled word will always differ mostly by one character. If the two words are the same, return True.
+
+// P: two strings, two words
+// R: determine if word2 differs from word1 by at most one character,
+//    if the count of differences is one or zero, return true,
+//    otherwise, return false
+// E: mispelled('versed', 'xersed'); // returns true
+//    mispelled('versed', 'applb'); // returns false
+//    mispelled('versed', 'v5rsed'); // returns true
+//    mispelled('1versed', 'versed'); // returns true
+//    mispelled('versed', 'versed'); // returns true
+// P:
+
+// create a function that takes in two strings, two words
+function mispelled(word1, word2) {
+    
+}
