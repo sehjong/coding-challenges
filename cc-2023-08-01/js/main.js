a326801b8768dc6b4870229ca854b5c05e41108c
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-07-31 10:27:24 -0500

    add attempt to coding challenge for 2023-08-01

--- cc-2023-08-01/js/main.js
+++ cc-2023-08-01/js/main.js
@@ -26,5 +26,14 @@
 
 // create a function that takes in two strings, two words
 function mispelled(word1, word2) {
-    
+    if (Math.abs(word1.length - word2.length) > 1) {
+        return false;
+    }
+
+    let diffCount = 0;
+    for (let i = 0; i < word1.length; i++) {
+        if (word1[i] !== word2[i]) {
+            diffCount++;
+        }
+    }
 }
