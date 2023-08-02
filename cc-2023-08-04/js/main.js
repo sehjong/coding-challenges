3601875feafe653bec8e2e66e43b9753d36d1a9b
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-08-02 09:57:59 -0500

    add details to coding challenge for 2023-08-04

---
+++ cc-2023-08-04/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+// Lot of museum allow you to be a member, for a certain amount amount_by_year you can have unlimitted acces to the museum.
+
+// In this kata you should complete a function in order to know after how many visit it will be better to take an annual pass. The function take 2 arguments annual_price and individual_price.
+
+// P: two numbers, annual_price and individual_price
+// R: the ratio or value to determine after how many visits it will be more cost-effective to take an annual pass
+// E: console.log(optimalVisits(40, 15), 3);
+//    console.log(optimalVisits(30, 10), 3);
+//    console.log(optimalVisits(80, 15), 6);
+// P:
+
+// create a function that takes in two numbers as parameters, annual_price and individual_price, as arguments
+function optimalVisits(annual_price, individual_price) {
+
+}
