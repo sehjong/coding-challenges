50dff1cc85ac1610b34232278c8e6a2a8e478db6
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-08-05 10:42:57 -0500

    add details to coding challenge for 2023-08-07

---
+++ cc-2023-08-07/js/main.js
@@ -0,0 +1,21 @@
+// PREP
+
+// Given an array (a list in Python) of integers and an integer n, find all occurrences of n in the given array and return another array containing all the index positions of n in the given array.
+
+// If n is not in the given array, return an empty array [].
+
+// Assume that n and all values in the given array will always be integers.
+
+// Example:
+
+// findAll([6, 9, 3, 4, 3, 82, 11], 3) => [2, 4]
+
+// P: an array of integers and an integer n
+// R: all occurrences of n in the given array and return another array containing all the index positions of n in the given array
+// E: findAll([6, 9, 3, 4, 3, 82, 11], 3) => [2, 4]
+// P:
+
+// create a function expression that takes an array of integers and an integer n
+const findAll = (arr, n) => {
+    
+}
