55286fa5a28815425aae3f6d7f63f1dfec9f6070
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-08-14 14:06:16 -0500

    add details to coding challenge for 2023-08-16

---
+++ cc-2023-08-16/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Complete the solution so that it returns the number of times the search_text is found within the full_text.
+
+// Usage example:
+
+// solution('aa_bb_cc_dd_bb_e', 'bb') # should return 2 since bb shows up twice
+// solution('aaabbbcccc', 'bbb') # should return 1
+
+// P: two string parameters, search_text and full_text
+// R: the number of times the search_text is found within the full_text
+// E: solution('aa_bb_cc_dd_bb_e', 'bb') # should return 2 since bb shows up twice
+//    solution('aaabbbcccc', 'bbb') # should return 1
+// P:
+
+// create a function named "solution" that takes two string arguments: full_text and search_text
+function solution(fullText, searchText) {
+    
+}
