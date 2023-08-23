84ff780c48e062c5ce8f7d339d7af53ddb0bac82
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-08-23 17:24:47 -0500

    add details to coding challenge for 2023-08-25

---
+++ cc-2023-08-25/js/main.js
@@ -0,0 +1,21 @@
+// PREP
+
+// Write a function that appends the items from sequence 2 onto sequence 1, returning the newly formed sequence. Your function should also be able to handle nested sequences.
+
+// All inputs will be arrays/nested arrays.
+
+// For example:
+
+// ['a','b','c'], [1,2,3]     --> ['a','b','c',1,2,3]
+// [['x','x'],'B'], ['c','D'] --> [['x','x'],'B','c','D']
+
+// P: two arrays
+// R: the newly formed appended array
+// E: ['a','b','c'], [1,2,3]     --> ['a','b','c',1,2,3]
+//    [['x','x'],'B'], ['c','D'] --> [['x','x'],'B','c','D']
+// P:
+
+// create a function that takes in two arrays
+function appendArrays(arr1, arr2) {
+    
+}
