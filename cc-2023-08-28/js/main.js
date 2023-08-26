1bffa25deb23b2bf7ae8dd52ad1bcb2cb192297b
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-08-26 14:16:45 -0500

    add details to coding challenge for 2023-08-28

---
+++ cc-2023-08-28/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+
+// Examples
+// ("hello", "world", "l")       ==>  "held"
+// ("coding", "anywhere", "n")   ==>  "codinywhere"
+// ("jason", "samson", "s")      ==>  "jasamson"
+// ("wonderful", "people", "e")  ==>  "wondeople"
+
+// P: three strings: two words and a letter
+// R: a single word that's a combination of both words, merged at the point where the given letter first appears in each word
+// E: ("hello", "world", "l")       ==>  "held"
+//    ("coding", "anywhere", "n")   ==>  "codinywhere"
+//    ("jason", "samson", "s")      ==>  "jasamson"
+//    ("wonderful", "people", "e")  ==>  "wondeople"
+// P:
+
+// create a function expression that takes in three parameters: word1, word2, and a letter
+const stringMerge = (word1, word2, letter) => {
+    
+}
