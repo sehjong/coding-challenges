e7ae43151e2ba67255f5247750843be90fed1a48
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-09-04 11:29:25 -0500

    add details to coding challenge for 2023-09-06

---
+++ cc-2023-09-06/js/main.js
@@ -0,0 +1,26 @@
+// PREP
+
+// You have to create a function named reverseIt.
+
+// Write your function so that in the case a string or a number is passed in as the data , you will return the data in reverse order. If the data is any other type, return it as it is.
+
+// Examples of inputs and subsequent outputs:
+
+// "Hello" -> "olleH"
+
+// "314159" -> "951413"
+
+// [1,2,3] -> [1,2,3]
+
+// P: any data type
+// R: if the input data is a string or a number, return the reverse of that data,
+//    if the input data is of any other data type, return it as it is
+// E: // "Hello" -> "olleH"
+      // "314159" -> "951413"
+      // [1,2,3] -> [1,2,3]
+// P:
+
+// create a function that takes in a data type
+function reverseIt(data) {
+
+}
