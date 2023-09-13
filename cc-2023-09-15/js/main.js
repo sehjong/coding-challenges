49b0d1da771dc588f42c7d2129ac55024b768059
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-09-13 13:45:10 -0500

    add details to coding challenge for 2023-09-15

---
+++ cc-2023-09-15/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Create a method that accepts an array of names, and returns an array of each name with its first letter capitalized.
+
+// example
+
+// capitalizeFirstLetter(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
+// capitalizeFirstLetter(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kelsey']
+
+// P: an array of names
+// R: an array of each name with its first letter capitalized
+// E: capitalizeFirstLetter(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
+//    capitalizeFirstLetter(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kelsey']
+// P:
+
+// create a function that takes in an array of names
+function capitalizeFirstLetter(names) {
+    
+}
