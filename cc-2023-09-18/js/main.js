16192c40e7491cb96d7bdba74095d619a4860d63
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-09-16 10:28:59 -0500

    add details to coding challenge for 2023-09-18

---
+++ cc-2023-09-18/js/main.js
@@ -0,0 +1,21 @@
+// PREP
+
+// A non-empty array a of length n is called an array of all possibilities if it contains all numbers between [0,a.length-1].Write a method named isAllPossibilities that accepts an integer array and returns true if the array is an array of all possibilities, else false.
+
+// Example:
+
+// a=[1,2,0,3]
+// a.length-1=3 
+// a includes [0,3] ,hence the function should return true
+
+// P: an integer array containing elements
+// R: true if the array is an "array of all possibilities," otherwise false
+// E: a=[1,2,0,3]
+//    a.length-1=3 
+//    a includes [0,3] ,hence the function should return true
+// P:
+
+// create a function called isAllPossibilities that takes in an array of integers
+function isAllPossibilities(arr) {
+
+}
