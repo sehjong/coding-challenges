e0ebc6d9c454c29aff1bd4511291de4e69f2d92b
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-09-19 10:07:04 -0500

    add details to coding challenge for 2023-09-21

---
+++ cc-2023-09-21/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+// Your job is to add a method 'sum' to all array objects, which sums all the numbers in it. You should return 0 if the array is empty.
+
+// For example:
+
+// var arr = [1,2,3,4];
+// arr.sum();  //returns 10
+
+// var arr = [10, 28, 14, 33];
+// arr.sum(); //returns 85
+// Good luck!
+
+// P: the function 'sum' should be added as a method
+// R: the sum of all the numbers in the array, if the array is empty, it should return 0
+// E: var arr = [1,2,3,4];
+//    arr.sum();  //returns 10
+//    var arr = [10, 28, 14, 33];
+//    arr.sum(); //returns 85
+// P:
+
+// create a method called 'sum' to the prototype of the Array object
+Array.prototype.sum = function () {
+
+}
