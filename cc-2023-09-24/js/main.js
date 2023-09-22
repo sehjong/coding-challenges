56d6fc66ff5f6973452369d58623b29ed948977e
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-09-22 17:30:36 -0500

    add details to coding challenge for 2023-09-24

---
+++ cc-2023-09-24/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Given a sequence of integers, return the sum of all the integers that have an even index (odd index in COBOL), multiplied by the integer at the last index.
+
+// Indices in sequence start from 0.
+
+// If the sequence is empty, you should return 0.
+
+// P: an array of integers
+// R: the sum of all the integers that have an even index, multiplied by the integer at the last index
+// E: console.log(evenLast([2, 3, 4, 5]), 30);
+//    console.log(evenLast([2, 2, 2, 2]), 8);
+//    console.log(evenLast([]), 0);
+// P:
+
+// create a function that takes in an array of numbers
+function evenLast(arrNums) {
+    
+}
