765d6ec15520914d4ccdd0a357d3b5d43a84caa8
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-09-26 13:51:41 -0500

    add attempt to coding challenge for 2023-09-27

--- cc-2023-09-27/js/main.js
+++ cc-2023-09-27/js/main.js
@@ -17,5 +17,15 @@
 
 // create a function that takes in an array
 function calculateScore(x) {
-    
+    let score = 0;
+
+    if (typeof x[0] === 'number' && typeof x[1] === 'number') {
+        score = x[0] + x[1];
+    } else if (typeof x[0] === 'number') {
+        score = x[0];
+    } else if (typeof x[1] === 'number') {
+        score = x[1];
+    } else {
+        return 'Void!';
+    }
 }
