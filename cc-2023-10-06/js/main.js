6af1d21ee25ee0fbf651dc3873119c69c1be308c
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-10-04 10:57:44 -0500

    add details to coding challenge for 2023-10-06

---
+++ cc-2023-10-06/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// When given a string of space separated words, return the word with the longest length. If there are multiple words with the longest length, return the last instance of the word with the longest length.
+
+// Example:
+
+// 'red white blue' //returns string value of white
+
+// 'red blue gold' //returns gold
+
+// P: string of space-separated words
+// R: the word with the longest length
+// E: 'red white blue' //returns string value of white
+//    'red blue gold' //returns gold
+// P:
+
+// create a function that takes in a string of space-separated words
+function longestWord(stringOfWords) {
+
+}
