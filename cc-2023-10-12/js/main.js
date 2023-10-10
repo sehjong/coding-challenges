0540e7a9d5066e01fd41cbff35aa41e70ca47308
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-10-10 12:29:07 -0500

    add details to coding challenge for 2023-10-12

---
+++ cc-2023-10-12/js/main.js
@@ -0,0 +1,29 @@
+// PREP
+
+// In this Kata, you will be given a lower case string and your task will be to remove k characters from that string using the following rule:
+
+// - first remove all letter 'a', followed by letter 'b', then 'c', etc...
+// - remove the leftmost character first.
+// For example: 
+// solve('abracadabra', 1) = 'bracadabra' # remove the leftmost 'a'.
+// solve('abracadabra', 2) = 'brcadabra'  # remove 2 'a' from the left.
+// solve('abracadabra', 6) = 'rcdbr'      # remove 5 'a', remove 1 'b' 
+// solve('abracadabra', 8) = 'rdr'
+// solve('abracadabra',50) = ''
+// More examples in the test cases. Good luck!
+
+// P: string, lowercase
+// R: the string according to the given rules:
+//    - first remove all letter 'a', followed by letter 'b', then 'c', etc.,
+//    - remove the leftmost character first
+// E: solve('abracadabra', 1) = 'bracadabra' # remove the leftmost 'a'.
+//    solve('abracadabra', 2) = 'brcadabra'  # remove 2 'a' from the left.
+//    solve('abracadabra', 6) = 'rcdbr'      # remove 5 'a', remove 1 'b' 
+//    solve('abracadabra', 8) = 'rdr'
+//    solve('abracadabra',50) = ''
+// P:
+
+// create a function that takes in a string
+function solve(str, k) {
+
+}
