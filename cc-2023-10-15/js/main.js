1f75a8bcf0d87fe81a735c995982fe1545764022
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-10-13 16:11:49 -0500

    add details to coding challenge for 2023-10-15

---
+++ cc-2023-10-15/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Move every letter in the provided string forward 10 letters through the alphabet.
+
+// If it goes past 'z', start again at 'a'.
+
+// Input will be a string with length > 0.
+
+// P: a string with a length greater than 0
+// R: a string with each letter moved forward 10 letters through the alphabet
+// E: console.log(moveTen('testcase'), 'docdmkco');
+//    console.log(moveTen('hello world'), 'rovvy gybvn');
+//    console.log(moveTen('abcxyz'), 'klmhij');
+// P:
+
+// create a function that takes in a string with a length greater than 0
+function moveTen(str) {
+
+}
