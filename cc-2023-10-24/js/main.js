0949b61ec2bbd1e76e0d8a543e3ae7ca9e97660f
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-10-22 15:14:08 -0500

    add details to coding challenge for 2023-10-24

---
+++ cc-2023-10-24/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// Given a string as input, move all of its vowels to the end of the string, in the same order as they were before.
+
+// Vowels are (in this kata): a, e, i, o, u
+
+// Note: all provided input strings are lowercase.
+
+// Examples
+// "day"    ==>  "dya"
+// "apple"  ==>  "pplae"
+
+// P: a string
+// R: a new string where all the vowels from the original string are moved to the end while keeping their original order
+// E: "day"    ==>  "dya"
+//    "apple"  ==>  "pplae"
+// P:
+
+// create a function that takes in a string
+function moveVowel(input) {
+    
+}
