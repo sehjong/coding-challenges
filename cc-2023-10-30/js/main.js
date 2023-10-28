d13e06d57479449722be4fe3fbca8d077ebdb101
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-10-28 15:32:40 -0500

    add details to coding challenge for 2023-10-30

---
+++ cc-2023-10-30/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Create a function to lowercase all strings in an array. Non-string items should remain unchanged.
+
+// Example
+// arrayLowerCase(['Red', 'Green']) == ['red', 'green']
+// arrayLowerCase([1, 'Green']) == [1, 'green']
+
+// P: an array of strings
+// R: that array with all strings lowercased,
+//    and non-strings unchanged
+// E: arrayLowerCase(['Red', 'Green']) == ['red', 'green']
+//    arrayLowerCase([1, 'Green']) == [1, 'green']
+// P:
+
+// create a function that takes in an array of strings
+function arrayLowerCase(arr) {
+    
+}
