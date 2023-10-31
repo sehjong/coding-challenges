926a0c1ca61e776944808661e33d7c149bba648e
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-10-31 16:46:42 -0500

    add details to coding challenge for 2023-11-02

---
+++ cc-2023-11-02/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+// In this kata you have to find whether same elements of the string are grouped together.
+
+// Example
+
+// Input	Output
+// 112233	true
+// 11223311	false
+
+// Explanation
+// In first example same elements are grouped together: (11)(22)(33), hence it should return true
+// In the second example there are two different groups of 1's: (11)2233(11), hence it should return false
+
+// P: a string of numbers 0-9
+// R: true if the same elements are grouped together, false if not
+// E: Input	   Output
+//    112233   true
+//    11223311 false
+// P:
+
+// create a function expression that takes in a string of numbers 0-9
+const areSameElementsGrouped = str => {
+
+}
