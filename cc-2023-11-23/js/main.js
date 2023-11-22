e7cfd91039675e6ca9532aff8f29188405506a77
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-11-21 19:50:33 -0600

    add details to coding challenge for 2023-11-23

---
+++ cc-2023-11-23/js/main.js
@@ -0,0 +1,26 @@
+// PREP
+
+// Steps
+
+// Square the numbers that are greater than zero.
+// Multiply by 3 every third number.
+// Multiply by -1 every fifth number.
+// Return the sum of the sequence.
+// Example
+// { -2, -1, 0, 1, 2 } returns -6
+
+// 1. { -2, -1, 0, 1 * 1, 2 * 2 }
+// 2. { -2, -1, 0 * 3, 1, 4 }
+// 3. { -2, -1, 0, 1, -1 * 4 }
+// 4. -6
+// P.S.: The sequence consists only of integers. And try not to use "for", "while" or "loop" statements.
+
+// P: an array of integers
+// R: the sum of the modified sequence
+// E: { -2, -1, 0, 1, 2 } returns -6
+// P:
+
+// create a function that takes in an array of integers
+function sequenceSum(arrNums) {
+
+}
