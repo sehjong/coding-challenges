5435ae3ba546065c77220ca6e012e08f8bb8a206
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-11-27 19:30:43 -0600

    add details to coding challenge for 2023-11-29

---
+++ cc-2023-11-29/js/main.js
@@ -0,0 +1,27 @@
+// PREP
+
+// Given an array of integers (x), and a target (t), you must find out if any two consecutive numbers in the array sum to t. If so, remove the second number.
+
+// Example:
+
+// x = [1, 2, 3, 4, 5]
+// t = 3
+
+// 1+2 = t, so remove 2. No other pairs = t, so rest of array remains:
+
+// [1, 3, 4, 5]
+
+// Work through the array from left to right.
+
+// Return the resulting array.
+
+// P: an array of integers (x) and a target integer (t)
+// R: an array of integers (x) with the second number removed if it equals the target integer (t)
+// E: x = [1, 2, 3, 4, 5], t = 3 => [1, 3, 4, 5],
+// 1+2 = t, so remove 2. No other pairs = t, so rest of array remains
+// P:
+
+// create a function that an array of integers (x) and a target integer (t)
+function removeSecondNum(x, t) {
+
+}
