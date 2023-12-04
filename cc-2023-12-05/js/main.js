a5b57f9d9d8c258d8306e83be368933db796aebb
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-12-03 18:18:42 -0600

    add details to coding challenge for 2023-12-05

---
+++ cc-2023-12-05/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Create a function that takes an input String and returns a String, where all the uppercase words of the input String are in front and all the lowercase words at the end. The order of the uppercase and lowercase words should be the order in which they occur.
+
+// If a word starts with a number or special character, skip the word and leave it out of the result.
+
+// Input String will not be empty.
+
+// For an input String: "hey You, Sort me Already!" the function should return: "You, Sort Already! hey me"
+
+// P: a string
+// R: a string where uppercase words are at the beginning, followed by lowercase words
+// E: "hey You, Sort me Already!" -> "You, Sort Already! hey me"
+// P:
+
+// create a function that takes in a string
+function sortWords(str) {
+    
+}
