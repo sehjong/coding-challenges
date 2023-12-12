1399126a9d7081c2a449a7d6c58ab81d4763c0b1
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-12-12 12:28:25 -0600

    add details to coding challenge for 2023-12-14

---
+++ cc-2023-12-14/js/main.js
@@ -0,0 +1,21 @@
+// PREP
+
+// Given a string, you progressively need to concatenate the first letter from the left and the first letter to the right and "1", then the second letter from the left and the second letter to the right and "2", and so on.
+
+// If the string's length is odd drop the central element.
+
+// For example:
+
+// charConcat("abcdef")    == 'af1be2cd3'
+// charConcat("abc!def")   == 'af1be2cd3' // same result
+
+// P: a string
+// R: a concatenated string
+// E: charConcat("abcdef")    == 'af1be2cd3'
+//    charConcat("abc!def")   == 'af1be2cd3' // same result
+// P:
+
+// create a function that takes in a string
+function charConcat(str) {
+    
+}
