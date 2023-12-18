e1c0059a8eb0724242029fcc3705b1649c61ec16
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-12-18 11:40:28 -0600

    add details to coding challenge for 2023-12-20

---
+++ cc-2023-12-20/js/main.js
@@ -0,0 +1,24 @@
+// PREP
+
+// Extend the Array's built-in functions by implementing .max() method. This method will return the largest number in the array. If the array contains one or more non-number elements that can't be converted into numbers (e.g., string letters), return NaN
+
+// Example:
+
+// [2,5,1,3].max() // returns 5
+// [1,2,3,8,4,9,7,42,99].max() // returns 99
+// [2,'5',1,3].max() // returns 5 ("5" can be converted to 5)
+// [2,5,1,'ab'].max() // returns NaN ("ab" can't be converted to a number)
+// Array will contain at least one item.
+
+// P: the function .max() should be added as a method to the prototype of the Array object
+// R: the largest number in the array
+// E: [2,5,1,3].max() // returns 5
+//    [1,2,3,8,4,9,7,42,99].max() // returns 99
+//    [2,'5',1,3].max() // returns 5 ("5" can be converted to 5)
+//    [2,5,1,'ab'].max() // returns NaN ("ab" can't be converted to a number)
+// P:
+
+// extend Array.prototype with a method .max() to create a function that returns the largest number in the array
+Array.prototype.max = function() {
+
+}
