96cbd59e9b7a54111c1e6807a8bc1c9bc5debe2a
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-12-21 16:39:07 -0600

    add details to coding challenge for 2023-12-23

---
+++ cc-2023-12-23/js/main.js
@@ -0,0 +1,32 @@
+// PREP
+
+// Your task is to take a given range and return an array of the prime numbers in that range.
+
+// You will write a function with the following parameters:
+
+// start is the integer from which your range starts. (inclusive)
+
+// end is the integer at which your range ends. (inclusive)
+
+// If the range does not contain any prime numbers return null.
+
+// Examples:
+
+// primes(5, 7)  // [5, 7]
+// primes(8, 9)  // null
+// primes(1, 10) // [2, 3, 5, 7]
+
+// P: an integer representing the start of the range (inclusive),
+//    and an integer representing the end of the range (inclusive)
+// R: an array of prime numbers within the specified range,
+//    if there are no prime numbers, return null
+// E: primes(5, 7)  // [5, 7]
+//    primes(8, 9)  // null
+//    primes(1, 10) // [2, 3, 5, 7]
+// P:
+
+// create a function expression that takes in an integer representing the start of the range (inclusive),
+// and an integer representing the end of the range (inclusive)
+const primes = (start, end) => {
+
+}
