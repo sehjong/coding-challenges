e4c241b8697c7685301bdb48f747fb241dd53887
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-12-24 20:05:03 -0600

    add details to coding challenge for 2023-12-26

---
+++ cc-2023-12-26/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Write a function that returns the number of arguments it received.
+
+// args_count() --> 0
+// args_count('a') --> 1
+// args_count('a', 'b') --> 2
+
+// P: a variable number of arguments
+// R: an integer representing the count of arguments received by the function as parameters
+// E: args_count() --> 0
+//    args_count('a') --> 1
+//    args_count('a', 'b') --> 2
+// P:
+
+// create a function args_count() that takes a variable number of arguments
+function args_count() {
+    
+}
