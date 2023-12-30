8f0cccb92b9bbe3450409c401f778309618cb933
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2023-12-29 22:07:33 -0600

    add solution to coding challenge for 2023-12-29

--- cc-2023-12-29/js/main.js
+++ cc-2023-12-29/js/main.js
@@ -51,3 +51,10 @@ function combine() {
 // return the combined object
     return obj;
 }
+
+const objA = { a: 10, b: 20, c: 30 };
+const objB = { a: 3, c: 6, d: 3 };
+const objC = { a: 5, d: 11, e: 8 };
+console.log(combine(objA, objB), { a: 13, b: 20, c: 36, d: 3 });
+console.log(combine(objA, objC), { a: 15, b: 20, c: 30, d: 11, e: 8 });
+console.log(combine(objB, objC), { a: 8, c: 6, d: 14, e: 8 });
