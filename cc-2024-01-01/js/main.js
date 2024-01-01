031648e555a89b598c361335c3e69f913cc9f0ab
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-01-01 12:54:18 -0600

    add solution to coding challenge for 2024-01-01

--- cc-2024-01-01/js/main.js
+++ cc-2024-01-01/js/main.js
@@ -24,3 +24,8 @@
 function oddOne(arr) {
     return arr.findIndex(num => num % 2 != 0);
 }
+
+console.log(oddOne([2, 4, 6, 7, 10]), 3);
+console.log(oddOne([2, 16, 98, 10, 13, 78]), 4);
+console.log(oddOne([4, -8, 98, -12, -7, 90, 100]), 4);
+console.log(oddOne([2, 4, 6, 8]), -1);
