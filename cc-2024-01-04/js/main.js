d59d45a63a46c8674bbbae081ba7d5af88ca0b46
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-01-04 14:08:22 -0600

    add solution to coding challenge for 2024-01-04

--- cc-2024-01-04/js/main.js
+++ cc-2024-01-04/js/main.js
@@ -35,3 +35,7 @@ function squares(x, n) {
 // return the array containing the sequence
     return result;
 }
+
+console.log(squares(2, 5), [2, 4, 16, 256, 65536]);
+console.log(squares(3, 3), [3, 9, 81]);
+console.log(squares(5, 3), [5, 25, 625]);
