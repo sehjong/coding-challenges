588e0cac4370fdd06e7e49282aa4e502417399bb
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-01-05 16:12:16 -0600

    add details to coding challenge for 2024-01-07

---
+++ cc-2024-01-07/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+// P: an array of integers
+// R: the values of the input array that are not odd,
+//    (i.e., only even numbers, including zero),
+//    in the same order as they appear in the input array
+// E: console.log(noOdds([0, 1]), [0]);
+//    console.log(noOdds([0, 1, 2, 3]), [0, 2]);
+//    console.log(noOdds([-3, -2, -1, 0, 1, 2, 3]), [-2, 0, 2]);
+// P:
+
+// create a function expression that takes in an array of integers
+let noOdds = values => {
+
+}
