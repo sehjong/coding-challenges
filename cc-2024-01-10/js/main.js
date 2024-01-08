6e3a03ce069fcd02ee5d4f362a02b82642c0e3ac
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-01-08 12:45:44 -0600

    add details to coding challenge for 2024-01-10

---
+++ cc-2024-01-10/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+// You must create a function, spread, that takes a function and a list of arguments to be applied to that function. You must make this function return the result of calling the given function/lambda with the given arguments.
+
+// eg:
+
+// spread(someFunction, [1, true, "Foo", "bar"] ) 
+// // is the same as...
+// someFunction(1, true, "Foo", "bar")
+
+// P: the function spread takes in two parameters:
+//    a function that will be called with a list of arguments,
+//    and an array containing those arguments to be applied to that function
+// R: the result of calling the function with the arguments provided
+// E: spread(someFunction, [1, true, "Foo", "bar"] ) 
+//    is the same as...
+//    someFunction(1, true, "Foo", "bar")
+// P:
+
+// create or define a function with two parameters,
+// a function that will be called with a list of arguments,
+// and an array containing those arguments to be applied to that function
+function spread(func, args) {
+    
+}
