6a31c22d940d5141bbd5920b42e8d83ecb683fa0
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-01-20 11:30:06 -0600

    add details to coding challenge for 2024-01-22

---
+++ cc-2024-01-22/js/main.js
@@ -0,0 +1,24 @@
+// PREP
+
+// Make the double() function return a new array with each value twice as large as the corresponding value in the passed in array. Your solution must use the map() function of the built-in javascript Array object.
+
+// function double(array) {
+//     // Use array.map() to return a new array with each value twice
+//     // as large as the corresponding value in the passed in array.
+// }
+// For example:
+
+// var someNumbers = [1,2,10,57];
+
+// double(someNumbers); // should return [2,4,20,114]
+
+// P: an array of numbers
+// R: a new array with each value twice as large as the corresponding value using the map() method on the passed in array
+// E: var someNumbers = [1,2,10,57];
+//    double(someNumbers); // should return [2,4,20,114]
+// P:
+
+// create a function that takes in an array of numbers
+function double(array) {
+
+}
