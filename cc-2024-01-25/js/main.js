bdddcf50e26e13746e9addebe2538faf7b70f053
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-01-23 11:53:27 -0600

    add details to coding challenge for 2024-01-25

---
+++ cc-2024-01-25/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+// There are some stones on Bob's table in a row, and each of them can be red, green or blue, indicated by the characters R, G, and B.
+
+// Help Bob find the minimum number of stones he needs to remove from the table so that the stones in each pair of adjacent stones have different colors.
+
+// Examples:
+
+// "RGBRGBRGGB"   => 1
+// "RGGRGBBRGRR"  => 3
+// "RRRRGGGGBBBB" => 9
+
+// P: a string representing a row of stones, where each stone is indicated by R, G, and B,
+//    for the colors red, green, and blue
+// R: a number that represents the minimum number of stones that need to be removed so that no two adjacent stones have the same color
+// E: "RGBRGBRGGB"   => 1
+//    "RGGRGBBRGRR"  => 3
+//    "RRRRGGGGBBBB" => 9
+// P:
+
+// create a functio that take in a string representing a row of stones, where each stone is indicated by R, G, and B,
+// for the colors red, green, and blue
+function solve(stones) {
+    
+}
