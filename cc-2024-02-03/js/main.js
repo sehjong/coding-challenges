2882df698a183d24546851498e2d1678396f0513
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-02-01 12:35:56 -0600

    add details to coding challenge for 2024-02-03

---
+++ cc-2024-02-03/js/main.js
@@ -0,0 +1,29 @@
+// PREP
+
+// You will get an array of numbers.
+
+// Every preceding number is smaller than the one following it.
+
+// Some numbers will be missing, for instance:
+
+// [-3,-2,1,5] //missing numbers are: -1,0,2,3,4
+// Your task is to return an array of those missing numbers:
+
+// [-1,0,2,3,4]
+
+// P: an array of numbers,
+//    where every preceding number is smaller than the one following it,
+//    and some numbers will be missing
+// R: an array containing all the missing numbers in the sequence,
+//    sorted in ascending order
+// E: [-3,-2,1,5] //missing numbers are: -1,0,2,3,4
+//    Your task is to return an array of those missing numbers:
+//    [-1,0,2,3,4]
+// P:
+
+// create a function that takes in an array of numbers,
+// where every preceding number is smaller than the one following it,
+// and some numbers will be missing
+function findMissingNumbers(arrNums) {
+
+}
