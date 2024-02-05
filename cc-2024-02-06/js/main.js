1983080a5450962cab2f8168f59515e826ed7097
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-02-04 18:30:28 -0600

    add details to coding challenge for 2024-02-06

---
+++ cc-2024-02-06/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// A colleague asked if you can help him fix his function. It seems it doesn't always parse Integers correctly.
+
+// Examples:
+
+// parseNumbers(['10']) // should return [10]
+// parseNumbers(['-1','0','1']) // should return [-1,0,1]
+
+// P: an array of integers in the form of strings
+// R: an array of integers converted from string format to integer format
+// E: parseNumbers(['10']) // should return [10]
+//    parseNumbers(['-1','0','1']) // should return [-1,0,1]
+// P:
+
+// create a function that takes in an array of integers in the form of strings
+function parseNumbers(intStr) {
+    
+}
