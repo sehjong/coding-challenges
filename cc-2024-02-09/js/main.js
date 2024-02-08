54e9a33ff3033de921d2c9215b3ec8f0ab9a2ccd
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-02-08 15:46:01 -0600

    add attempt to coding challenge for 2024-02-09

--- cc-2024-02-09/js/main.js
+++ cc-2024-02-09/js/main.js
@@ -30,7 +30,11 @@
 // P:
 
 // create a function expression that takes in an array of integers whose elements have both a negative and a positive value, except for one integer,
-// the only-positive or only-negative integer may appear more than once
-const solve = intArr => {
-
-}
+// the only-positive or only-negative integer may appear more than once,
+// iterate over the array using the find method to search for the first element in the array,
+// that does not have an opposite sign counterpart, and implicitly return that integer,
+// given the coding challenge's requirements, finding the first element once, which is a single unique integer, satisfies the condition,
+// even if this integer appears more than once, and is sufficient to solve the problem,
+// stopping the search after finding the first occurrence of the unique integer makes the function more efficient,
+// especially for large arrays
+const solve = intArr => intArr.find(e => !intArr.includes(-e));
