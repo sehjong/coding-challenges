d3657ff34a4e8ef0a38bf717c062eaff3c3b72ca
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-02-10 10:09:11 -0600

    add details to coding challenge for 2024-02-12

---
+++ cc-2024-02-12/js/main.js
@@ -0,0 +1,40 @@
+// PREP
+
+// You have to search all numbers from inclusive 1 to inclusive a given number x, that have the given digit d in it.
+// The value of d will always be 0 - 9.
+// The value of x will always be greater than 0.
+
+// You have to return as an array
+
+// the count of these numbers,
+// their sum
+// and their product.
+
+// For example:
+// x = 11
+// d = 1
+// ->
+// Numbers: 1, 10, 11
+// Return: [3, 22, 110]
+
+// If there are no numbers, which include the digit, return [0,0,0].
+
+// P: a number x representing the upper limit of the range to search within, inclusive,
+//    the value of which will always be greater than 0,
+//    and a number d between 0 and 9, inclusive, representing the digit to search for within the numbers
+// R: an array containing the count of numbers within the range 1 to x that contain the digit d,
+//    the sum of these numbers, and the product of these numbers,
+//    if no numbers are found, the product is 0
+// E: x = 11
+//    d = 1
+//    ->
+//    Numbers: 1, 10, 11
+//    Return: [3, 22, 110]
+// P: 
+
+// create a function that takes in a number x representing the upper limit of the range to search within, inclusive,
+// the value of which will always be greater than 0,
+// and a number d between 0 and 9, inclusive, representing the digit to search for within the numbers
+function findNums(x, d) {
+
+}
