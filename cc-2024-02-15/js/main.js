ac0369b1783cb0f0c119b14fcd5aba1914607191
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-02-13 11:36:09 -0600

    add details to coding challenge for 2024-02-15

---
+++ cc-2024-02-15/js/main.js
@@ -0,0 +1,47 @@
+// PREP
+
+// Task
+// Given a Divisor and a Bound , Find the largest integer N , Such That ,
+
+// Conditions :
+// N is divisible by divisor
+
+// N is less than or equal to bound
+
+// N is greater than 0.
+
+// Notes
+// The parameters (divisor, bound) passed to the function are only positive values .
+// It's guaranteed that a divisor is Found .
+// Input >> Output Examples
+// divisor = 2, bound = 7 ==> return (6)
+// Explanation:
+// (6) is divisible by (2) , (6) is less than or equal to bound (7) , and (6) is > 0 .
+
+// divisor = 10, bound = 50 ==> return (50)
+// Explanation:
+// (50) is divisible by (10) , (50) is less than or equal to bound (50) , and (50) is > 0 .*
+
+// divisor = 37, bound = 200 ==> return (185)
+// Explanation:
+// (185) is divisible by (37) , (185) is less than or equal to bound (200) , and (185) is > 0 .
+
+// P: the parameters (divisor, bound) passed to the function are only positive values greater than 0,
+//    whereby the number N is divisible by divisor and N is less than or equal to the upper limit of bound
+// R: a number N that is divisible by divisor, less than or equal to bound, and greater than 0
+// E: divisor = 2, bound = 7 ==> return (6)
+//    Explanation:
+//    (6) is divisible by (2) , (6) is less than or equal to bound (7) , and (6) is > 0 .
+//
+//    divisor = 10, bound = 50 ==> return (50)
+//    Explanation:
+//    (50) is divisible by (10) , (50) is less than or equal to bound (50) , and (50) is > 0 .
+//
+//    divisor = 37, bound = 200 ==> return (185)
+//    Explanation:
+//    (185) is divisible by (37) , (185) is less than or equal to bound (200) , and (185) is > 0 .
+// P:
+
+function maxMultiple(divisor, bound) {
+
+}
