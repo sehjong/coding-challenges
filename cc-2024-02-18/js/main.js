9d36c14ffc3fd27947743579ec89fb936829d156
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-02-16 17:45:17 -0600

    add details to coding challenge for 2024-02-18

---
+++ cc-2024-02-18/js/main.js
@@ -0,0 +1,24 @@
+// PREP
+
+// Is every value in the array an array?
+
+// This should only test the second array dimension of the array. The values of the nested arrays don't have to be arrays.
+
+// Examples:
+
+// [[1],[2]] => true
+// ['1','2'] => false
+// [{1:1},{2:2}] => false
+
+// P: an array
+// R: true if every element within the array is itself an array,
+//    false if at least one element within the array is not an array
+// E: [[1],[2]] => true
+//    ['1','2'] => false
+//    [{1:1},{2:2}] => false
+// P:
+
+// create a function expression that takes in an array
+const arrCheck = arr => {
+    
+}
