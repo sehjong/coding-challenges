0939eb58638540a1e339f783495990f35d320965
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-02-22 13:17:31 -0600

    add details to coding challenge for 2024-02-24

---
+++ cc-2024-02-24/js/main.js
@@ -0,0 +1,31 @@
+// PREP
+
+// I have the par value for each hole on a golf course and my stroke score on each hole. I have them stored as strings, because I wrote them down on a sheet of paper. Right now, I'm using those strings to calculate my golf score by hand: take the difference between my actual score and the par of the hole, and add up the results for all 18 holes.
+
+// For example:
+
+// If I took 7 shots on a hole where the par was 5, my score would be: 7 - 5 = 2
+// If I got a hole-in-one where the par was 4, my score would be: 1 - 4 = -3.
+// Doing all this math by hand is really hard! Can you help make my life easier?
+
+// Task Overview
+// Complete the function which accepts two strings and calculates the golf score of a game. Both strings will be of length 18, and each character in the string will be a number between 1 and 9 inclusive.
+
+// P: a string of length 18, parList, where each character represents the par value of each hole on a golf course,
+//    with characters being numbers between 1 and 9 inclusive,
+//    and a string of the same length as parList, scoreList, where each character represents the actual strokes taken on each hole,
+//    with characters being numbers between 1 and 9 inclusive
+// R: an integer which represents the total difference between the player's strokes and the par values for all 18 holes,
+//    it could be positive, negative, or zero, indicating how many strokes over, under, or exactly at par the player was, respectively
+// E: console.log(golfScoreCalculator('7', '5'), 2);
+//    console.log(golfScoreCalculator('4', '1'), -3);
+//    console.log(golfScoreCalculator('123', '321'), 0);
+// P:
+
+// create a function that takes in a string of length 18, parList, where each character represents the par value of each hole on a golf course,
+//    with characters being numbers between 1 and 9 inclusive,
+//    and a string of the same length as parList, scoreList, where each character represents the actual strokes taken on each hole,
+//    with characters being numbers between 1 and 9 inclusive
+function golfScoreCalculator(parList, scoreList) {
+
+}
