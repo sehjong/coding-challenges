e0781c4f78f56ddbb1cc53b2db7828936448fa59
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-02-23 13:37:27 -0600

    add attempt to coding challenge for 2024-02-24

--- cc-2024-02-24/js/main.js
+++ cc-2024-02-24/js/main.js
@@ -17,15 +17,18 @@
 //    with characters being numbers between 1 and 9 inclusive
 // R: an integer which represents the total difference between the player's strokes and the par values for all 18 holes,
 //    it could be positive, negative, or zero, indicating how many strokes over, under, or exactly at par the player was, respectively
-// E: console.log(golfScoreCalculator('7', '5'), 2);
+// E: console.log(golfScoreCalculator('5', '7'), 2);
 //    console.log(golfScoreCalculator('4', '1'), -3);
 //    console.log(golfScoreCalculator('123', '321'), 0);
 // P:
 
-// create a function that takes in a string of length 18, parList, where each character represents the par value of each hole on a golf course,
+// create a function expression that takes in a string of length 18, parList, where each character represents the par value of each hole on a golf course,
 //    with characters being numbers between 1 and 9 inclusive,
 //    and a string of the same length as parList, scoreList, where each character represents the actual strokes taken on each hole,
 //    with characters being numbers between 1 and 9 inclusive
-function golfScoreCalculator(parList, scoreList) {
-
-}
+const golfScoreCalculator = (parList, scoreList) =>
+    // use the reduce method to iterate over each character in scoreList, which represents scores,
+    // for each score, convert it and the corresponding par to numbers, subtract them,
+    // add the difference to the accumulating result, and return the new result,
+    // initialize the accumulator (result) to 0 at the start of the reduction
+    [...scoreList].reduce((result, score, index) => result += score - parList[index], 0);
