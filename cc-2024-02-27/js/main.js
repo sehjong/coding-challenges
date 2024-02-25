eea6e3607bd3a8c2726507c778e5581380b8975c
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-02-25 16:16:06 -0600

    add details to coding challenge for 2024-02-27

---
+++ cc-2024-02-27/js/main.js
@@ -0,0 +1,29 @@
+// PREP
+
+// Create a function strCount (takes an object as argument) that will count all string values inside an object. For example:
+
+// strCount({
+//   first: "1",
+//   second: "2",
+//   third: false,
+//   fourth: ["anytime",2,3,4],
+//   fifth:  null
+//   })
+//   //returns 3
+
+// P: an object that can contain various types of values, including nested objects
+// R: an integer representing the total count of values within the object, including nested objects, that are of the string type
+// E: strCount({
+//    first: "1",
+//    second: "2",
+//    third: false,
+//    fourth: ["anytime",2,3,4],
+//    fifth:  null
+//    })
+//    //returns 3
+// P:
+
+// create a function that takes in an object that can contain various types of values, including nested objects
+function strCount(obj) {
+    
+}
