ebf2173e530a72250786d20cc38034f971479a04
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-02-26 17:13:29 -0600

    add attempt to coding challenge for 2024-02-27

--- cc-2024-02-27/js/main.js
+++ cc-2024-02-27/js/main.js
@@ -25,5 +25,19 @@
 
 // create a function that takes in an object that can contain various types of values, including nested objects
 function strCount(obj) {
-    
+    // initialize a counter for string values
+    let count = 0;
+
+    // iterate over each key in the object
+    for (key in obj) {
+        // check if the current value is a string,
+        // if it's a string, increment the count
+        if (typeof obj[key] == 'string') count++;
+        // check if the current value is an object,
+        // if it's an object, recursively call strCount on it and add the result to the count
+        if (typeof obj[key] == 'object') count += strCount(obj[key]);
+    }
+
+    // return the total count of strings found in the object
+    return count;
 }
