6994e4be19d1d6d54826147ae777e22022b30e7e
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-02-27 17:31:38 -0600

    add solution to coding challenge for 2024-02-27

--- cc-2024-02-27/js/main.js
+++ cc-2024-02-27/js/main.js
@@ -29,15 +29,26 @@ function strCount(obj) {
     let count = 0;
 
     // iterate over each key in the object
-    for (key in obj) {
-        // check if the current value is a string,
-        // if it's a string, increment the count
-        if (typeof obj[key] == 'string') count++;
-        // check if the current value is an object,
-        // if it's an object, recursively call strCount on it and add the result to the count
-        if (typeof obj[key] == 'object') count += strCount(obj[key]);
+    for (const key in obj) { // declare key with const for block scope
+        // check if the current value is a string
+        if (typeof obj[key] === 'string') {
+            count++; // if it's a string, increment the count
+          // check if the current value is an object and not null
+        } else if (typeof obj[key]=== 'object' && obj[key] !== null) {
+            // if it's an object, and not null, recursively count strings within it,
+            // and add the result to the count
+            count += strCount(obj[key]);
+        }
     }
 
     // return the total count of strings found in the object
     return count;
 }
+
+console.log(strCount({
+    first: "1",
+    second: "2",
+    third: false,
+    fourth: ["anytime",2,3,4],
+    fifth:  null
+}), 3);
