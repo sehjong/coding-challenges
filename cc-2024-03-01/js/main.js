1f66af7e57a20936fb16211b1f3f72f2fa6f1ea3
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-02-28 13:47:40 -0600

    add details to coding challenge for 2024-03-01

---
+++ cc-2024-03-01/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+// Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+
+// The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+
+// P: a string
+// R: true if the string contains only unique characters, false otherwise
+// E: console.log(hasUniqueChars('  nAa'), false);
+//    console.log(hasUniqueChars('abcdef'), true);
+//    console.log(hasUniqueChars('++-'), false);
+// P:
+
+// create a function expression that takes in a string
+let hasUniqueChars = (str) => {
+    
+}
