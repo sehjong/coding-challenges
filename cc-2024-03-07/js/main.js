3382f92a99f51314cc79c9ed0fcd0ee0b8bcbe41
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-03-05 18:24:50 -0600

    add details to coding challenge for 2024-03-07

---
+++ cc-2024-03-07/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+// An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+// Example:
+
+// The starting array sequence is [1,2,3,4,5,6,7,8,9]
+// The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+// Your function should return the int 5.
+// If no number was deleted from the starting array, your function should return the int 0.
+
+// Note: N may be 1 or less (in the latter case, the first array will be []).
+
+// P: an array of numbers arrNums and a mixed array of numbers arrNumsMix derived from arrNums
+// R: the number that was deleted from arrNums to obtain arrNumsMix,
+//    if no number was deleted, the function returns 0
+// E: console.log(findDeletedNum([1,2,3,4,5,6,7,8,9], [3,2,4,6,7,8,1,9]), 5);
+//    console.log(findDeletedNum([1,2,3,4,5,6,7,8,9], [3,2,4,6,7,8,1,9,5]), 0);
+//    console.log(findDeletedNum([], []), 0);
+// P:
+
+// create a function that takes in an array of numbers arrNums and a mixed array of numbers arrNumsMix derived from arrNums
+function findDeletedNum(arrNums, arrNumsMix) {
+
+}
