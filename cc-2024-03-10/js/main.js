20938070a41211ef267a816b49d452f41b6007b1
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-03-09 05:18:52 -0600

    add attempt to coding challenge for 2024-03-10

--- cc-2024-03-10/js/main.js
+++ cc-2024-03-10/js/main.js
@@ -3,7 +3,7 @@
 // Write reverseList function that simply reverses lists.
 
 // P: an array of elements of any type
-// R: a new array that is the reverse of the input array
+// R: an array that is the reverse of the input array
 // E: console.log(reverseList([1, 2, 3, 4]), [4, 3, 2, 1]);
 //    console.log(reverseList([4, 'orange', 2, 'sky']), ['sky', 2, 'orange', 4]);
 //    console.log(reverseList([]), []);
@@ -11,5 +11,6 @@
 
 // create a function that takes in an array of elements of any type
 function reverseList(arr) {
-
+    // use the reverse method to reverse the array in place and return it
+    return arr.reverse();
 }
