c4cb01eaeaa3e612418aceb6c8d8bd59d606d2cd
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-03-12 19:26:52 -0500

    add attempt to coding challenge for 2024-03-13

--- cc-2024-03-13/js/main.js
+++ cc-2024-03-13/js/main.js
@@ -19,7 +19,12 @@
 //    leastLarger( [4, 1, 3, 5, 6], 4 )  => -1
 // P:
 
-// create a function expression that takes in an array of numbers and an index within that array
-const leastLarger = (arr, i) => {
-
-}
+// create a function expression that takes in an array of numbers and an index within that array,
+// use the indexOf method to find and return the index of the smallest number in the array,
+// that is larger than the element at the provided index, this process involves
+// applying the spread syntax "..." to expand elements of a filtered array where each element is, 
+// filtered to include only those that are larger than the element at the given index,
+// Math.min is then used to calculate the smallest number among these larger elements,
+// if no such element exists (meaning all elements are smaller or equal), indexOf returns -1,
+// indicating the absence of a larger element
+const leastLarger = (arr, i) => arr.indexOf(Math.min(...arr.filter(e => e > arr[i])));
