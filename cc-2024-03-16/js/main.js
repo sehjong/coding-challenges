80ef49b7423c54d36108c58e5d8aefde472258aa
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-03-14 15:09:02 -0500

    add details to coding challenge for 2024-03-16

---
+++ cc-2024-03-16/js/main.js
@@ -0,0 +1,13 @@
+// PREP
+
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// P: an array of strings
+// R: an array with the elements sorted in lexicographical order
+// E: console.log(arrSort(['one', 'two', 'three']), ['one', 'three', 'two']);
+// P:
+
+// create a function expression that takes in an array of strings
+const arrSort = strArr => {
+
+}
