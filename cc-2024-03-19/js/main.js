39ba76859da979cb8af7439bbea62c621a839f31
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-03-17 17:47:38 -0500

    add details to coding challenge for 2024-03-19

---
+++ cc-2024-03-19/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// You are given an array of n+1 integers 1 through n. In addition there is a single duplicate integer.
+
+// The array is unsorted.
+
+// An example valid array would be [3, 2, 5, 1, 3, 4]. It has the integers 1 through 5 and 3 is duplicated. [1, 2, 4, 5, 5] would not be valid as it is missing 3.
+
+// You should return the duplicate value as a single integer.
+
+// P: an unsorted array of n + 1 integers 1 through n and there is one duplicate
+// R: the duplicate value as a single integer
+// E: console.log(findDuplicateInteger([3, 2, 5, 1, 3, 4]), 3);
+// P:
+
+// create a function that takes in an unsorted array of n + 1 integers 1 through n and there is one duplicate
+function findDuplicateInteger(arrNums) {
+    
+}
