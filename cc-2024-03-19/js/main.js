3808b2f8788d74039fae912b15c98c012f0a5704
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-03-18 18:06:15 -0500

    add attempt to coding challenge for 2024-03-19

--- cc-2024-03-19/js/main.js
+++ cc-2024-03-19/js/main.js
@@ -15,5 +15,17 @@
 
 // create a function that takes in an unsorted array of n + 1 integers 1 through n and there is one duplicate
 function findDuplicateInteger(arrNums) {
-    
+    // sort the array in ascending order
+    arrNums.sort();
+
+    // loop through the array
+    for(let i = 0; i < arrNums.length; i++) {
+        // check if the current element is equal to the next element
+        if(arrNums[i] == arrNums[i + 1]) {
+            // if a duplicate is found, return the duplicate value
+            return arrNums[i];
+        }
+    }
+
+    // if no duplicate is found by the end of the loop, implicitly return undefined
 }
