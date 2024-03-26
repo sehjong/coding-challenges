e25f0e696de257a35de5d28cf867b1570586f284
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-03-26 14:07:36 -0500

    add details to coding challenge for 2024-03-28

---
+++ cc-2024-03-28/js/main.js
@@ -0,0 +1,24 @@
+// PREP
+
+// In this Kata, you will be given two positive integers a and b and your task will be to apply the following operations:
+
+// i) If a = 0 or b = 0, return [a,b]. Otherwise, go to step (ii);
+// ii) If a ≥ 2*b, set a = a - 2*b, and repeat step (i). Otherwise, go to step (iii);
+// iii) If b ≥ 2*a, set b = b - 2*a, and repeat step (i). Otherwise, return [a,b].
+// a and b will both be lower than 10E8.
+
+// More examples in tests cases. Good luck!
+
+// P: a positive integer a and a positive integer b
+// R: an array of two integers, [a, b], after applying the specified operations,
+//    until neither operation can further modify the values
+// E: i) If a = 0 or b = 0, return [a,b]. Otherwise, go to step (ii);
+//    ii) If a ≥ 2*b, set a = a - 2*b, and repeat step (i). Otherwise, go to step (iii);
+//    iii) If b ≥ 2*a, set b = b - 2*a, and repeat step (i). Otherwise, return [a,b].
+//    a and b will both be lower than 10E8.
+// P:
+
+// create a function that takes in a positive integer a and a positive integer b
+function solve(a, b) {
+
+}
