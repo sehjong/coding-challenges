6a9df3746ad093afc40af0daecfe5b8530a90c52
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-03-29 14:31:18 -0500

    add details to coding challenge for 2024-03-31

---
+++ cc-2024-03-31/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+// There exist two zeroes: +0 (or just 0) and -0.
+
+// Write a function that returns true if the input number is -0 and false otherwise (True and False for Python).
+
+// In JavaScript / TypeScript / Coffeescript the input will be a number.
+
+// In Python / Java / C / NASM / Haskell / the input will be a float.
+
+// P: a number
+// R: true if the input number is -0, and false otherwise
+// E: console.log(isNegativeZero(-5), false);
+//    console.log(isNegativeZero(0), false);
+//    console.log(isNegativeZero(-0), true);
+//    console.log(isNegativeZero(5), false);
+// P:
+
+// create a function expression that takes in a number
+const isNegativeZero = (num) => {
+
+}
