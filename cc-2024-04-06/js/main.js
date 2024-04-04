5c23c7f3d342ae46ba611e7ff804b6e4b93d0b0d
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-04-04 18:10:38 -0500

    add details to coding challenge for 2024-04-06

---
+++ cc-2024-04-06/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+// Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+// Example:
+
+// Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+// P: a two-dimensional array of integers
+// R: the flattened version of the array with all the integers in the sorted (ascending) order
+// E: Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// P:
+
+// create a function that takes in a two-dimensional array of integers
+function flattenAndSort(arr) {
+    
+}
