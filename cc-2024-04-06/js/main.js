f91017cb2f9f4a8fa7604b28f3f76e32fdea19cd
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-04-06 18:30:03 -0500

    add solution to coding challenge for 2024-04-06

--- cc-2024-04-06/js/main.js
+++ cc-2024-04-06/js/main.js
@@ -17,3 +17,5 @@ function flattenAndSort(arr) {
     // use the sort() method to sort the new array in ascending order
     return [].concat(...arr).sort((a, b) => a - b);
 }
+
+console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
