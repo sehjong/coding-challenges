857107c69831df7e3f73f84e5ac039a1b0eb2cf3
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-04-07 18:56:15 -0500

    add details to coding challenge for 2024-04-09

---
+++ cc-2024-04-09/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+// (the dedicated builtin(s) functionalities are deactivated)
+
+// P: an array
+// R: a new array with the elements in reverse order,
+//    implemented manually since the dedicated built-in functionalities for reversing,
+//    are intentionally deactivated for this challenge
+// E: console.log(reverse([1, 2, 3, 4]), [4, 3, 2, 1]);
+//    console.log(reverse(['a', 'b', 'c']), ['c', 'b', 'a']);
+//    console.log(reverse([1, 'a', 2, 'b', 3, 'c']), ['c', 3, 'b', 2, 'a', 1]);
+// P:
+
+// create a function expression that takes in an array
+const reverse = arr => {
+
+}
