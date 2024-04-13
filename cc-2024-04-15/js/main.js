1e637ebda435978e12359079d1ce6a8d34bc8a7c
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-04-13 11:42:46 -0500

    add details to coding challenge for 2024-04-15

---
+++ cc-2024-04-15/js/main.js
@@ -0,0 +1,31 @@
+// PREP
+
+// Write a function that takes a list (in Python) or array (in other languages) of numbers, and makes a copy of it.
+
+// Note that you may have troubles if you do not return an actual copy, item by item, just a pointer or an alias for an existing list or array.
+
+// If not a list or array is given as a parameter in interpreted languages, the function should raise an error.
+
+// Examples:
+
+// t = [1, 2, 3, 4]
+// tCopy = copyList(t)
+// t[1] += 5
+// t = [1, 7, 3, 4]
+// tCopy = [1, 2, 3, 4]
+
+// P: an array of numbers
+// R: a new array that is a copy of the original array,
+//    each element from the original array is copied by value,
+//    ensuring that modifications to the original do not affect the copy
+// E: t = [1, 2, 3, 4]
+//    tCopy = copyList(t)
+//    t[1] += 5
+//    t = [1, 7, 3, 4]
+//    tCopy = [1, 2, 3, 4]
+// P:
+
+// create a function that takes in an array of numbers
+function copyArray(array) {
+
+}
