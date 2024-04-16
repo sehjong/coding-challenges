eaae3cabce4565fc88e0d67ab6c29fff64315a24
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-04-16 12:54:34 -0500

    add details to coding challenge for 2024-04-18

---
+++ cc-2024-04-18/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+// In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number groupings and return the largest number. Numbers will not have leading zeros.
+
+// For example, solve("gh12cdy695m1") = 695, because this is the largest of all number groupings.
+
+// Good luck!
+
+// P: a single string consisting of lowercase letters and numbers
+// R: the largest number found in the string,
+//    if no numbers are found, return 0
+// E: solve("gh12cdy695m1") = 695, because this is the largest of all number groupings
+// P:
+
+function solve(str) {
+    
+}
