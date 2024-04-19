ac10edb7829b52ba934a48d5a117b841a1a5ec03
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-04-19 15:35:26 -0500

    add details to coding challenge for 2024-04-21

---
+++ cc-2024-04-21/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+// Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+
+// Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+
+// solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+// See test cases for more examples.
+
+// Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
+
+// Good luck!
+
+// P: an array of strings, where each string is a word composed of alphabet letters,
+//    both uppercase and lowercase letters may be included
+// R: an array of numbers, each number represents the count of letters in the corresponding word from the input array,
+//    that are in the same position as they are in the alphabet
+// E: solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+// P:
+
+// create a function that takes in an array of strings, where each string is a word composed of alphabet letters,
+// both uppercase and lowercase letters may be included
+function solve(arr) {
+    
+}
