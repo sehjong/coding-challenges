08f8c014e56e1f39a15285fc5d3b9cfad86fb7b4
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-04-22 13:56:07 -0500

    add details to coding challenge for 2024-04-24

---
+++ cc-2024-04-24/js/main.js
@@ -0,0 +1,16 @@
+// PREP
+
+// Please write a function that sums a list, but ignores any duplicate items in the list.
+
+// For instance, for the list [3, 4, 3, 6] , the function should return 10.
+
+// P: an array of numbers, which may include duplicate values
+// R: the sum of all unique numbers in the array,
+//    a number is considered unique if it appears exactly once in the array
+// E: [3, 4, 3, 6] -> 10
+// P:
+
+// create a function that takes in an array of numbers, which may include duplicate values
+function sumUniqueNumbers(arrNums) {
+
+}
