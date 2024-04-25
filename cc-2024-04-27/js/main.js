d6bdca8fda1d48407ac9c699ce754807e16af891
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-04-25 15:42:50 -0500

    add details to coding challenge for 2024-04-27

---
+++ cc-2024-04-27/js/main.js
@@ -0,0 +1,31 @@
+// PREP
+
+// You will be given an array that contains two strings. Your job is to create a function that will take those two strings and transpose them, so that the strings go from top to bottom instead of left to right.
+
+// e.g. transposeTwoStrings(['Hello','World']);
+
+// should return
+
+// H W  
+// e o  
+// l r  
+// l l  
+// o d
+
+// P: an array containing exactly two strings
+// R: a single string that represents the two input strings transposed,
+//    each character from the strings is separated by a space and placed vertically,
+//    if one string is shorter, spaces are used to align with the longer string's characters
+// E: e.g. transposeTwoStrings(['Hello','World']);
+//    should return
+//    H W  
+//    e o  
+//    l r  
+//    l l  
+//    o d
+// P:
+
+// create a function expression that takes in an array containing exactly two strings
+const transposeTwoStrings = arrStr => {
+
+}
