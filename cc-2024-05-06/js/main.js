4622b7cee6e36ee77f7e0abf974779154bee9409
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-05-04 09:15:59 -0500

    add details to coding challenge for 2024-05-06

---
+++ cc-2024-05-06/js/main.js
@@ -0,0 +1,34 @@
+// PREP
+
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4.
+
+// P: an array of the numbers 0 and 1 of varying length
+// R: the binary number represented by the array
+// E: Testing: [0, 0, 0, 1] ==> 1
+//    Testing: [0, 0, 1, 0] ==> 2
+//    Testing: [0, 1, 0, 1] ==> 5
+//    Testing: [1, 0, 0, 1] ==> 9
+//    Testing: [0, 0, 1, 0] ==> 2
+//    Testing: [0, 1, 1, 0] ==> 6
+//    Testing: [1, 1, 1, 1] ==> 15
+//    Testing: [1, 0, 1, 1] ==> 11
+// P:
+
+// create a function expression that takes in an array of the numbers 0 and 1 of varying length
+const parseBinary = arrNums => {
+
+}
