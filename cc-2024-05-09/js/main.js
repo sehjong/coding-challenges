11878b1a76cdd7c0593a516275e7c20a3f11ac0c
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-05-09 13:08:17 -0500

    add solution to coding challenge for 2024-05-09

--- cc-2024-05-09/js/main.js
+++ cc-2024-05-09/js/main.js
@@ -21,3 +21,7 @@ function repeats(arrNums) {
     // sum the elements that occur exactly once
     return arrNums.filter(e => arrNums.indexOf(e) === arrNums.lastIndexOf(e)).reduce((acc, c) => acc + c, 0);
 }
+
+console.log(repeats([4, 5, 7, 5, 4, 8]), 15);
+console.log(repeats([9, 10, 19, 13, 19, 13]), 19);
+console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11]), 12);
