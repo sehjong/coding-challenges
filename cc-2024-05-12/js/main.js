aef541bc485551d537aae933ae049bb71f73dc1b
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-05-10 11:36:25 -0500

    add details to coding challenge for 2024-05-12

---
+++ cc-2024-05-12/js/main.js
@@ -0,0 +1,24 @@
+// PREP
+
+// Each floating-point number should be formatted that only the first two decimal places are returned. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+// Don't round the numbers! Just cut them after two decimal places!
+
+// Right examples:  
+// 32.8493 is 32.84  
+// 14.3286 is 14.32
+
+// Incorrect examples (e.g. if you round the numbers):  
+// 32.8493 is 32.85  
+// 14.3286 is 14.33
+
+// P: a float
+// R: only the first two decimal places
+// E: 32.8493 is 32.84  
+//    14.3286 is 14.32
+// P:
+
+// create a function expression that takes in a floated number
+const twoDecimalPlaces = num => {
+    
+}
