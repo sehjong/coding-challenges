3b570aafdad99fb7bf809a309ff4e10083f537ba
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-05-13 13:09:32 -0500

    add details to coding challenge for 2024-05-15

---
+++ cc-2024-05-15/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+// This kata is about converting numbers to their binary or hexadecimal representation:
+
+// If a number is even, convert it to binary.
+// If a number is odd, convert it to hex.
+// Numbers will be positive. The hexadecimal string should be lowercased.
+
+// P: a whole number
+// R: the binary representation if the number is even,
+//    the hexadecimal representation if the number is odd
+// E: console.log(evensAndOdds(2), 10);
+//    console.log(evensAndOdds(13), 'd');
+//    console.log(evensAndOdds(0), 0);
+// P:
+
+// create a function expression that takes in a whole number
+const evensAndOdds = num => {
+
+}
