07ecf8cfeb6009d8f438970a3aa2ed457662f420
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-05-16 13:53:28 -0500

    add details to coding challenge for 2024-05-18

---
+++ cc-2024-05-18/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+// Implement a function that returns the minimal and the maximal value of a list (in this order).
+
+// P: an array of integers
+// R: an array with two elements,
+//    the first element is the minimal value in the input array,
+//    the second element is the maximal value in the input array
+// E: console.log(getMinMax([1]), [1, 1]);
+//    console.log(getMinMax([1, 2]), [1, 2]);
+//    console.log(getMinMax([2, 1]), [1, 2]);
+// P:
+
+// create a function that takes in an array of integers
+function getMinMax(arrNums) {
+    
+}
