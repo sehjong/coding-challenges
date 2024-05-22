ce1ea684aad1f47e0a6af84e42c81acc92abeecf
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-05-22 17:29:09 -0500

    add details to coding challenge for 2024-05-24

---
+++ cc-2024-05-24/js/main.js
@@ -0,0 +1,30 @@
+// PREP
+
+// The museum of incredibly dull things
+// The museum of incredibly dull things wants to get rid of some exhibits. Miriam, the interior architect, comes up with a plan to remove the most boring exhibits. She gives them a rating, and then removes the one with the lowest rating.
+
+// However, just as she finished rating all exhibits, she's off to an important fair, so she asks you to write a program that tells her the ratings of the exhibits after removing the lowest one. Fair enough.
+
+// Task
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with the lowest index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+// Examples
+// * Input: [1,2,3,4,5], output = [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+// P: an array of integers
+// R: an array of integers with the first occurrence of the lowest rating removed,
+//    the original array remains unchanged,
+//    return an empty array if the input is empty
+// E: * Input: [1,2,3,4,5], output = [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+// P:
+
+// create a function that takes in an array of integers
+function removeMinimum(nums) {
+    
+}
