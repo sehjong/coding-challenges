28c29cc84a37fd43ee29e315380518c307161fc9
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-05-25 14:14:12 -0500

    add details to coding challenge for 2024-05-27

---
+++ cc-2024-05-27/js/main.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Write a function that reverses the bits in an integer.
+
+// For example, the number 417 is 110100001 in binary. Reversing the binary is 100001011 which is 267.
+
+// You can assume that the number is not negative.
+
+// P: a number
+// R: a number that represents the reverse binary of the input number
+// E: console.log(reverseBinary(417), 267);
+//    console.log(reverseBinary(267), 417);
+//    console.log(reverseBinary(0), 0);
+// P:
+
+// create a function that takes in a number
+function reverseBinary(num) {
+
+}
