da0eaf154cba492d7cb702149a38eece59bf72ee
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-05-31 09:54:31 -0500

    add details to coding challenge for 2024-06-02

---
+++ cc-2024-06-02/js/main.js
@@ -0,0 +1,29 @@
+// PREP
+
+// Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value "Fizz" instead
+// If the value is a multiple of 5: use the value "Buzz" instead
+// If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+// N will never be less than 1.
+
+// Method calling example:
+
+// fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+// P: a number
+// R: an array of numbers from 1 to N with the following substitutions,
+//    'Fizz' for multiples of 3,
+//    'Buzz' for multiples of 5,
+//    'FizzBuzz' for multiples of both 3 and 5
+// E: console.log(fizzbuzz(3), [1, 2, 'Fizz']);
+//    console.log(fizzbuzz(10), [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz']);
+//    console.log(fizzbuzz(20), [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz']);
+// P:
+
+// create a functon that takes in a number
+function fizzBuzz(num) {
+    
+}
