298f7cf94ef7a370ebc9f528b694a9dbdceaec6f
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-06-02 10:53:35 -0500

    add solution to coding challenge for 2024-06-02

--- cc-2024-06-02/js/main.js
+++ cc-2024-06-02/js/main.js
@@ -44,3 +44,7 @@ function fizzBuzz(num) {
 
     return result;
 }
+
+console.log(fizzBuzz(3), [1, 2, 'Fizz']);
+console.log(fizzBuzz(10), [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz']);
+console.log(fizzBuzz(20), [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz']);
