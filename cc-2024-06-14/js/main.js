3c268b70a0ca85fade9c04965ee99678d4814e89
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-06-12 11:57:28 -0500

    add details to coding challenge for 2024-06-14

---
+++ cc-2024-06-14/js/main.js
@@ -0,0 +1,23 @@
+// PREP
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Additionally, if the number is negative, return 0.
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+*/
+
+// P: a number
+// R: the sum of all the multiples of 3 or 5 below the number passed in
+// E: console.log(solution(10), 23);
+//    console.log(solution(20), 78);
+//    console.log(solution(0), 0);
+// P:
+
+// create a function that takes in a number
+function solution(num) {
+
+}
