907bbf4d935169f491eb130ed4384a087f3557c5
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-06-17 16:49:26 -0500

    add solution to coding challenge for 2024-06-17

--- cc-2024-06-17/js/main.js
+++ cc-2024-06-17/js/main.js
@@ -38,3 +38,9 @@ function findOdd(arr) {
         }
     }
 }
+
+console.log(findOdd([7]), 7);
+console.log(findOdd([0]), 0);
+console.log(findOdd([1, 1, 2]), 2);
+console.log(findOdd([0, 1, 0, 1, 0]), 0);
+console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]), 4);
