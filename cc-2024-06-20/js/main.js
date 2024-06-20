59dd63dd727aaac7f18e8ce5219b5f7bed4d6a91
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-06-20 17:58:33 -0500

    add solution to coding challenge for 2024-06-20

--- cc-2024-06-20/js/main.js
+++ cc-2024-06-20/js/main.js
@@ -26,3 +26,6 @@ function arrayDiff(a, b) {
     // that are not present in b
     return a.filter(e => !b.includes(e));
 }
+
+console.log(arrayDiff([1, 2], [1]), [2]);
+console.log(arrayDiff([1, 2, 2, 2, 3], [2]), [1, 3]);
