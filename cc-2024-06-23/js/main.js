affed039a6596594ee5088c8b0ded87ae488e548
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-06-21 12:41:30 -0500

    add details to coding challenge for 2024-06-23

---
+++ cc-2024-06-23/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+/*
+The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+*/
+
+// P: a string
+// R: an object where the keys are the characters from the string,
+//    and the values are the counts of how many times each character appears in the string
+// E: console.log(count({}), {});
+//    console.log(count('ab'), {'a': 1, 'b': 1);
+//    console.log(count('aba'), {'a': 2, 'b': 1});
+// P:
+
+// create a function that takes in a string
+function count(str) {
+    
+}
