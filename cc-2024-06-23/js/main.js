f89e1e860476a668b9cec8e2746e29f536203ec8
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-06-22 13:09:19 -0500

    add attempt to coding challenge for 2024-06-23

--- cc-2024-06-23/js/main.js
+++ cc-2024-06-23/js/main.js
@@ -9,12 +9,23 @@ What if the string is empty? Then the result should be empty object literal, {}.
 // P: a string
 // R: an object where the keys are the characters from the string,
 //    and the values are the counts of how many times each character appears in the string
-// E: console.log(count({}), {});
-//    console.log(count('ab'), {'a': 1, 'b': 1);
+// E: console.log(count(''), {});
+//    console.log(count('ab'), {'a': 1, 'b': 1});
 //    console.log(count('aba'), {'a': 2, 'b': 1});
 // P:
 
 // create a function that takes in a string
 function count(str) {
-    
+    // initialize an empty object to store character counts
+    const count = {};
+
+    // split the string into an array of characters and iterate over each character
+    str.split('').forEach(function(char) {
+        // if the character is already a key in the count object, increment its value by 1,
+        // otherwise, add the character as a key with a value of 1
+        count[char] ? count[char]++ : count[char] = 1;
+    })
+
+    // return the object containing character counts
+    return count;
 }
