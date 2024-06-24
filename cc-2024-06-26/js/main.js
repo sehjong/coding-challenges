dd7f64c8980d740b0026f4dd0877aa1927230fc9
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-06-24 16:53:45 -0500

    add details to coding challenge for 2024-06-26

---
+++ cc-2024-06-26/js/main.js
@@ -0,0 +1,32 @@
+// PREP
+
+/*
+Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+*/
+
+// P: a string
+// R: a number representing the count of distinct case-insensitive characters,
+//    and numeric digits that occur more than once in the input string
+// E: "abcde" -> 0 # no characters repeats more than once
+//    "aabbcde" -> 2 # 'a' and 'b'
+//    "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+//    "indivisibility" -> 1 # 'i' occurs six times
+//    "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+//    "aA11" -> 2 # 'a' and '1'
+//    "ABBA" -> 2 # 'A' and 'B' each occur twice
+// P:
+
+// create a function that takes in a string
+function duplicateCount(str) {
+
+}
