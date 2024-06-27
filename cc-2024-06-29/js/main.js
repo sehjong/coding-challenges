a573f89eec890785daec3c19cb3b7342443f4920
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-06-27 17:29:13 -0500

    add details to coding challenge for 2024-06-29

---
+++ cc-2024-06-29/js/main.js
@@ -0,0 +1,24 @@
+// PREP
+
+/*
+Task
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+*/
+
+// P: an array of numbers
+// R: a new array where all the odd numbers are sorted in ascending order,
+//    but the even numbers remain in their original positions
+// E: [7, 1]  =>  [1, 7]
+//    [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+//    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+// P:
+
+// create a function that takes in an array of numbers
+function sortArray(array) {
+
+}
