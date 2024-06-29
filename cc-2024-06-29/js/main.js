ccd4faafc8452743b6953976baaba08fd6e9ff1d
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-06-29 17:49:00 -0500

    add solution to coding challenge for 2024-06-29

--- cc-2024-06-29/js/main.js
+++ cc-2024-06-29/js/main.js
@@ -25,3 +25,7 @@ function sortArray(array) {
     // map the original array, replacing odd numbers with sorted ones
     return array.map((e) => e % 2 ? odd.shift() : e);
 }
+
+console.log(sortArray([7, 1]), [1, 7]);
+console.log(sortArray([5, 8, 6, 3, 4]), [3, 8, 6, 5, 4]);
+console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]), [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]);
