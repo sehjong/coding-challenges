958bf09352cf369606900c5f7f92ffcefe372f76
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-06-30 14:22:16 -0500

    add details to coding challenge for 2024-07-02

---
+++ cc-2024-07-02/js/main.js
@@ -0,0 +1,29 @@
+// PREP
+
+/*
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+For example, the score of test is 64 (20 + 5 + 19 + 20).
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+*/
+
+// P: a string of words
+// R: the string representing the word with the highest score,
+//    each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.,
+//    if two words have the same score, the word that appears first in the input string is returned
+// E: console.log(highestScoringWord('abc cba'), 'abc');
+//    console.log(highestScoringWord('test', 'case'), 'test');
+//    console.log(highestScoringWord('this is a test sentence with words of varying scores'), 'varying');
+// p:
+
+// create a function that takes in a string of words
+function highestScoringWord(str) {
+
+}
