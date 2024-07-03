2c6a180222eed0a8758e25f9ad91541e309c5047
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-07-03 13:48:56 -0500

    add details to coding challenge for 2024-07-05

---
+++ cc-2024-07-05/js/main.js
@@ -0,0 +1,28 @@
+// PREP
+
+/*
+You get an array of arrays.
+If you sort the arrays by their length, you will see, that their length-values are consecutive.
+But one array is missing!
+
+
+You have to write a method, that return the length of the missing array.
+
+Example:
+[[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+
+If the array of arrays is null/nil or empty, the method should return 0.
+
+When an array in the array is null or empty, the method should return 0 too!
+There will always be a missing element and its length will be always between the given arrays.
+*/
+
+// P: an array of arrays
+// R: a number representing the length of the missing array
+// E: [[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+// P:
+
+// create a function that takes in an array of arrays
+function findMissingArray(nestedArrays) {
+    
+}
