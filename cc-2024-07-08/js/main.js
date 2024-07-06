956044be898f0b7cd0b41545cd1f5cfc3fe258d1
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-07-06 14:55:41 -0500

    add details to coding challenge for 2024-07-08

---
+++ cc-2024-07-08/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+/*
+Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+
+Note: no empty arrays will be given.
+
+Examples
+[12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+[12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+[12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+*/
+
+// P: an array of numbers
+// R: the number which is most frequent in the given input array,
+//    if there is a tie for most frequent number, return the largest number among them
+// E: [12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+//    [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+//    [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+// P:
+
+// create a function that takes in an array of numbers
+function mostFrequentNum(arrNums) {
+    
+}
