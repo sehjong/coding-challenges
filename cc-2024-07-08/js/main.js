d15d91a47365476473eefb2ff56dba413c112427
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-07-07 15:29:23 -0500

    add attempt to coding challenge for 2024-07-08

--- cc-2024-07-08/js/main.js
+++ cc-2024-07-08/js/main.js
@@ -21,5 +21,31 @@ Examples
 
 // create a function that takes in an array of numbers
 function mostFrequentNum(arrNums) {
-    
+    // initialize an empty object to store frequency counts
+    let obj = {};
+
+    // iterate over each element in the input array
+    arrNums.forEach(function(e) {
+        // if the element is not already a key in the object, initialize it with a value of 0
+        if (obj[e] === undefined)
+            obj[e] = 0;
+        // increment the count for the element
+        obj[e]++;
+    })
+
+    // get the keys, which are the unique elements, from the frequency object
+    let keys = Object.keys(obj), highest = 0, key;
+
+    // iterate over the keys to find the element with the highest frequency
+    keys.forEach(function(e) {
+        // if the current element's frequency is higher than the highest recorded frequency,
+        // or if it's equal and the current element is larger, update the highest count and corresponding key
+        if (obj[e] >= highest) {
+            highest = obj[e];
+            key = e;
+        }
+    })
+
+    // return the most frequent element as a number
+    return parseInt(key);
 }
