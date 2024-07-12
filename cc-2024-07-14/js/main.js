4cdb1d39cc13910d50f90f73b7bfb636f8d36d62
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-07-12 12:46:28 -0500

    add details to coding challenge for 2024-07-14

---
+++ cc-2024-07-14/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+/*
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+*/
+
+// P: an array of numbers
+// R: the unique number from the input array
+// E: findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+//    findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// P:
+
+// create a function that takes in an array of numbers
+function findUniq(arr) {
+    
+}
