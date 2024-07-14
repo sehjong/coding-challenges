8d2935d044931b5b718915940c1c4b6a2d72641b
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-07-14 12:53:31 -0500

    add solution to coding challenge for 2024-07-14

--- cc-2024-07-14/js/main.js
+++ cc-2024-07-14/js/main.js
@@ -24,3 +24,6 @@ function findUniq(arr) {
     // if they are the same, return the last element; otherwise, return the first element
     return arr[0] === arr[1] ? arr[arr.length - 1] : arr[0];
 }
+
+console.log(findUniq([1, 1, 1, 2, 1, 1]), 2);
+console.log(findUniq([0, 0, 0.55, 0, 0]), 0.55);
