c7094901160770395067727758152c0d5760d41d
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-07-17 13:38:23 -0500

    add solution to coding challenge for 2024-07-17

--- cc-2024-07-17/js/main.js
+++ cc-2024-07-17/js/main.js
@@ -64,3 +64,8 @@ function findEvenIndex(arr) {
     // return -1 if no such index is found
     return -1;
 }
+
+console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1]), 3);
+console.log(findEvenIndex([1, 100, 50, -51, 1 ,1]), 1);
+console.log(findEvenIndex([20, 10, -80, 10, 10, 15, 35]), 0);
+console.log(findEvenIndex([1, 2, 3, 4, 5, 6]), -1);
