2bed79c7c5e437521cd54c7d39a59e0dfe3db41b
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-07-18 14:10:00 -0500

    add details to coding challenge for 2024-07-20

---
+++ cc-2024-07-20/js/main.js
@@ -0,0 +1,28 @@
+// PREP
+
+/*
+Return the century of the input year. The input will always be a 4 digit string, so there is no need for validation.
+
+Examples
+"1999" --> "20th"
+"2011" --> "21st"
+"2154" --> "22nd"
+"2259" --> "23rd"
+"1124" --> "12th"
+"2000" --> "20th"
+*/
+
+// P: a 4 digit string representing a year
+// R: the century of the input year
+// E: "1999" --> "20th"
+//    "2011" --> "21st"
+//    "2154" --> "22nd"
+//    "2259" --> "23rd"
+//    "1124" --> "12th"
+//    "2000" --> "20th"
+// P:
+
+// create a function that takes in a 4 digit string representing a year
+function whatCentury(year) {
+    
+}
