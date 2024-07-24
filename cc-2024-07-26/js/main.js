ff0737a1520eccf239b2c6c30b0efcda4e99697e
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-07-24 10:39:57 -0500

    add details to coding challenge for 2024-07-26

---
+++ cc-2024-07-26/js/main.js
@@ -0,0 +1,26 @@
+// PREP
+
+/*
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+Input = "The sunset sets at twelve o' clock."
+Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+*/
+
+// P: a string
+// R: a string where each letter in the input string is replaced with its corresponding position in the alphabet
+// E: Input = "The sunset sets at twelve o' clock."
+//    Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+// P:
+
+// create a function that takes in a string
+function alphabetPositions(str) {
+    
+}
