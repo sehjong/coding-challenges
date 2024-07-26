3336d414ca99aab075e9d21df865082b6c5158e8
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-07-26 11:00:37 -0500

    add solution to coding challenge for 2024-07-26

--- cc-2024-07-26/js/main.js
+++ cc-2024-07-26/js/main.js
@@ -38,3 +38,5 @@ function alphabetPositions(str) {
     // trim any trailing space from the result string and return it
     return result.trim();
 }
+
+console.log(alphabetPositions("The sunset sets at twelve o' clock."), '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11');
