04ff551fcc28fd1c8879490365291b5b73dbc192
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-07-27 15:53:32 -0500

    add details to coding challenge for 2024-07-29

---
+++ cc-2024-07-29/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+/*
+Find the missing letter
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e'
+['O','Q','R','S'] -> 'P'
+*/
+
+// P: an array of letters
+// R: a string representing the missing letter in the array
+// E: ['a','b','c','d','f'] -> 'e'
+//    ['O','Q','R','S'] -> 'P'
+// P:
+
+// create a function that takes in an array of letters
+function missingLetter(arr) {
+    
+}
