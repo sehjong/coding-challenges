50100ec64c3578861c8ecb6e8f03dc1e01501f05
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-07-30 12:12:49 -0500

    add details to coding challenge for 2024-08-01

---
+++ cc-2024-08-01/js/main.js
@@ -0,0 +1,37 @@
+// PREP
+
+/*
+Your task, is to create N×N multiplication table, of size provided in parameter.
+
+For example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9
+For the given example, the return value should be:
+
+[[1,2,3],[2,4,6],[3,6,9]]
+*/
+
+// P: a number representing the size of a 2D matrix,
+//    that is, the number of rows and columns of a 2D array of n * n
+// R: a matrix where each subarray represents a row in the multiplication table
+// E: 
+/*
+if the number is 3,
+
+1 2 3
+2 4 6
+3 6 9
+
+->
+
+[[1,2,3],[2,4,6],[3,6,9]]
+*/
+// P:
+
+// create a function expression that takes in a number representing the size of a 2D matrix,
+// that is, the number of rows and columns of a 2D array of n * n
+let multiplicationTable(size) {
+
+}
