4b870ec15fa2c8671f6924d706900ff8f19a7ceb
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-08-02 15:07:58 -0500

    add details to coding challenge for 2024-08-04

---
+++ cc-2024-08-04/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+/*
+Write a function with the signature shown below:
+
+function isIntArray(arr) {
+  return true
+}
+returns true  / True if every element in an array is an integer or a float with no decimals.
+returns true  / True if array is empty.
+returns false / False for every other input.
+*/
+
+// P: an array
+// R: true if all elements in the array are integers or floats with no decimals, or if the array is empty,
+//    false for every other input
+// E: console.log(isIntArray([1, 2, 3, 4]), true);
+//    console.log(isIntArray(), true);
+//    console.log(isIntArray(['1']), false);
+// P:
+
+// create a function that takes in an array
+function isIntArray(arr) {
+    
+}
