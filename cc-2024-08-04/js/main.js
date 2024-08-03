a5259114fa53130af8660bd67f633d8fa7160adc
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-08-03 15:16:24 -0500

    add attempt to coding challenge for 2024-08-04

--- cc-2024-08-04/js/main.js
+++ cc-2024-08-04/js/main.js
@@ -15,11 +15,15 @@ returns false / False for every other input.
 // R: true if all elements in the array are integers or floats with no decimals, or if the array is empty,
 //    false for every other input
 // E: console.log(isIntArray([1, 2, 3, 4]), true);
-//    console.log(isIntArray(), true);
+//    console.log(isIntArray([]), true);
 //    console.log(isIntArray(['1']), false);
 // P:
 
 // create a function that takes in an array
 function isIntArray(arr) {
-    
+    // check if the input is an array and if all its elements are integers
+    return Array.isArray(arr) && arr.every(function(x) {
+        // check if the element is an integer by comparing it to its floored value
+        return Math.floor(x) === x;
+    })
 }
