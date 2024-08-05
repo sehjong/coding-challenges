5a6580f9150b90e2aed402f6ea68eaeb7016469c
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-08-05 11:27:08 -0500

    add details to coding challenge for 2024-08-07

---
+++ cc-2024-08-07/js/main.js
@@ -0,0 +1,36 @@
+// PREP
+
+/*
+Your task is to finish two functions, minimumSum and maximumSum, that take 2 parameters:
+
+values: an array of integers with an arbitrary length; may be positive and negative
+n: how many integers should be summed; always 0 or bigger
+Example:
+var values = [5, 4, 3, 2, 1];
+minimumSum(values, 2); // should return 1+2 = 3
+maximumSum(values, 3); // should return 3+4+5 = 12
+All values given to the functions will be integers. Also take care of the following special cases:
+
+if values is empty, both functions should return 0
+if n is 0, both functions should also return 0
+if n is larger than values's length, use the length instead.
+*/
+
+// P: an array of integers, and a whole number
+// R: the sum of the smallest and largest integers in the array
+// E: var values = [5, 4, 3, 2, 1];
+//    minimumSum(values, 2); // should return 1+2 = 3
+//    maximumSum(values, 3); // should return 3+4+5 = 12
+// P:
+
+// create a function that takes in an array of integers, and a whole number,
+// to calculate the minimum sum of the smallest integers in an array
+function minimumSum(values, n) {
+
+}
+
+// create a function that takes in an array of integers, and a whole number,
+// to calculate the maximum sum of the largest integers in an array
+function maximumSum(values, n) {
+    
+}
