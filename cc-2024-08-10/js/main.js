0302a6bfd59103c0aa47c3ee4db84f975ff99a5d
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-08-08 12:14:17 -0500

    add details to coding challenge for 2024-08-10

---
+++ cc-2024-08-10/js/main.js
@@ -0,0 +1,25 @@
+// PREP
+
+/*
+Write a function called findUnique which returns the only unique number from an array.
+
+All numbers in the unsorted array are present twice, except the one you have to find. The numbers are always valid integer values between 1 and 2147483647, so no need for type and error checking. The array contains at least one number and may contain millions of numbers. So make sure your solution is optimized for speed.
+
+Example
+Input:
+
+[ 1, 8, 4, 4, 6, 1, 8 ]
+Expected output:
+
+6
+*/
+
+// P: an array of numbers
+// R: the unique number from the array
+// E: [ 1, 8, 4, 4, 6, 1, 8 ] -> 6
+// P:
+
+// create a function that takes in an array of numbers
+function findUnique(num) {
+
+}
