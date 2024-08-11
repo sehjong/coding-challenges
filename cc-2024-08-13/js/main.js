4077c8785f28bab1c73d5141567fc2e5031f5ce6
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-08-11 18:04:48 -0500

    add details to coding challenge for 2024-08-13

---
+++ cc-2024-08-13/js/main.js
@@ -0,0 +1,23 @@
+// PREP
+
+/*
+In this kata, you will sort elements in an array by decreasing frequency of elements. If two elements have the same frequency, sort them by increasing value.
+
+solve([2,3,5,3,7,9,5,3,7]) = [3,3,3,5,5,7,7,2,9]
+-- We sort by highest frequency to lowest frequency.
+-- If two elements have same frequency, we sort by increasing value.
+More examples in test cases.
+
+Good luck!
+*/
+
+// P: an array of numbers
+// R: an array of numbers sorted by decreasing frequency,
+//    and by increasing value when two elements have the same frequency
+// E: solve([2,3,5,3,7,9,5,3,7]) = [3,3,3,5,5,7,7,2,9]
+// P:
+
+// create a function that takes in an array of numbers
+function solve(arr) {
+    
+}
