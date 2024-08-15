ad9d960bf6129337b1b0a2ae5df03185d77c940e
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-08-14 19:09:24 -0500

    add details to coding challenge for 2024-08-16

---
+++ cc-2024-08-16/js/main.js
@@ -0,0 +1,22 @@
+// PREP
+
+/*
+Given a string, remove any characters that are unique from the string.
+
+Example:
+
+input: "abccdefee"
+
+output: "cceee"
+*/
+
+// P: a string
+// R: a string containing only the characters that appear more than once in the input string,
+//    and in the order they originally appeared
+// E: 'abccdefee' -> 'cceee'
+// P:
+
+// create a function that takes in a string
+function onlyDuplicates(str) {
+    
+}
