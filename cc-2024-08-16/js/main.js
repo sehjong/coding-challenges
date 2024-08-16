312a299a2956970583bf7061452394d7e4f8b54a
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-08-15 19:17:52 -0500

    add attempt to coding challenge for 2024-08-16

--- cc-2024-08-16/js/main.js
+++ cc-2024-08-16/js/main.js
@@ -18,5 +18,8 @@ output: "cceee"
 
 // create a function that takes in a string
 function onlyDuplicates(str) {
-    
+    // split the string into an array of characters,
+    // filter out characters that appear only once,
+    // then join the remaining characters back into a string
+    return str.split('').filter(e => str.indexOf(e) != str.lastIndexOf(e)).join('');
 }
