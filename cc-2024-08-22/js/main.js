c5209006b6d669a40bdd09ac2c9b286a4b0a9f38
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-08-20 15:19:39 -0500

    add attempt to coding challenge for 2024-08-22

---
+++ cc-2024-08-22/js/main.js
@@ -0,0 +1,24 @@
+// PREP
+
+/*
+In this kata, your goal is to write a function which will reverse the vowels in a string. Any characters which are not vowels should remain in their original position. Here are some examples:
+
+"Hello!" => "Holle!"
+"Tomatoes" => "Temotaos"
+"Reverse Vowels In A String" => "RivArsI Vewols en e Streng"
+For simplicity, you can treat the letter y as a consonant, not a vowel.
+
+Good luck!
+*/
+
+// P: a string
+// R: a new string where the vowels have been reversed, while non-vowel characters remain unchanged
+// E: "Hello!" => "Holle!"
+//    "Tomatoes" => "Temotaos"
+//    "Reverse Vowels In A String" => "RivArsI Vewols en e Streng"
+// P:
+
+// create a function expression that takes in a string
+const reverseVowels = (str) => {
+    
+}
