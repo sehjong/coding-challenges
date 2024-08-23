12fe0d1fee64358c7d09cc4d9aed9eb31399543b
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-08-23 09:09:06 -0500

    add details to coding challenge for 2024-08-25

---
+++ cc-2024-08-25/js/main.js
@@ -0,0 +1,21 @@
+// PREP
+
+/*
+Given a string of integers, return the number of odd-numbered substrings that can be formed.
+
+For example, in the case of "1341", they are 1, 1, 3, 13, 41, 341, 1341, a total of 7 numbers.
+
+solve("1341") = 7. See test cases for more examples.
+
+Good luck!
+*/
+
+// P: a string
+// R: a number representing the number of odd-numbered substrings that can be formed from the string
+// E: solve("1341") = 7
+// P:
+
+// create a function that takes in a string
+function solve(str) {
+
+}
