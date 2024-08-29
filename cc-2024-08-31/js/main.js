f8faf7fb5d54c1232a0ce2971e4a2d765b9638e3
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-08-29 12:12:13 -0500

    add details to coding challenge for 2024-08-31

---
+++ cc-2024-08-31/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+/*
+Build a function sumNestedNumbers/sum_nested_numbers that finds the sum of all numbers in a series of nested arrays raised to the power of their respective nesting levels. Numbers in the outer most array should be raised to the power of 1.
+
+For example,
+
+sumNestedNumbers([1, [2], 3, [4, [5]]])
+should return 1 + 2*2 + 3 + 4*4 + 5*5*5 === 149
+*/
+
+// P: an array of nested numbers and a current nesting level, defaulting to 1
+// R: the sum of all numbers, with each number raised to the power of its respective nesting level
+// E: [1, [2], 3, [4, [5]]] -> 149
+// P:
+
+// create a function that takes in an array of nested numbers and a current nesting level, defaulting to 1
+function sumNestedNumbers(arr, level = 1) {
+
+}
