3d7766b831155685af6bd1cc3965bb8713a5c2d9
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-09-01 11:35:57 -0500

    add details to coding challenge for 2024-09-03

---
+++ cc-2024-09-03/js/main.js
@@ -0,0 +1,16 @@
+// PREP
+
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+// P: a boolean input, which can either be true or false
+// R: a string, either 'Yes' if the input is true, or 'No' if the input is false
+// E: true -> 'Yes'
+//    false -> 'No'
+// P:
+
+// create a function that takes in a boolean input, which can either be true or false
+function booleanToString(value) {
+
+}
