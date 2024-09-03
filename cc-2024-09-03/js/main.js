88ecd2b52f46de70e56f4e17efb3f81c6a3f1b13
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-09-03 12:16:51 -0500

    add solution to coding challenge for 2024-09-03

--- cc-2024-09-03/js/main.js
+++ cc-2024-09-03/js/main.js
@@ -12,7 +12,10 @@ Complete the method that takes a boolean value and return a "Yes" string for tru
 
 // create a function that takes in a boolean input, which can either be true or false
 function booleanToString(value) {
-    // use the ternary operator to check if the input is true or false
+    // use the ternary operator to check if the input is true or false,
     // return 'Yes' if the input is true, and 'No' if the input is false
     return value ? 'Yes' : 'No';
 }
+
+console.log(booleanToString(true), 'Yes');
+console.log(booleanToString(false), 'No');
