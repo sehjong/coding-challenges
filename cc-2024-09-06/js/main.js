d8d099fbc8085914b6717bd523957e5db2616cdd
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-09-04 13:40:00 -0500

    add details to coding challenge for 2024-09-06

---
+++ cc-2024-09-06/js/main.js
@@ -0,0 +1,18 @@
+// PREP
+
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+// P: a boolean input, which can either be true or false
+// R: a string representation of the boolean input 'true' or 'false'
+// E: console.log(booleanToString(true), 'true');
+//    console.log(booleanToString(false), 'false');
+// P:
+
+// create a function that takes in a boolean input, which can either be true or false
+function booleanToString(value) {
+    
+}
