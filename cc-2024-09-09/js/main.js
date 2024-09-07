a521634273ebe8420cb23ff091f96dee1cc595b2
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-09-07 14:16:47 -0500

    add details to coding challenge for 2024-09-09

---
+++ cc-2024-09-09/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+/*
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+*/
+
+// P: a number
+// R: the number multiplied by 8 if even, or 9 if odd
+// E: console.log(multiplyEvenOdd(2), 16);
+//    console.log(multiplyEvenOdd(1), 9);
+//    console.log(multiplyEvenOdd(8), 64);
+// P:
+
+// create a function that takes in a number
+function multiplyEvenOdd(num) {
+
+}
