0dd89e84859314d93afbd1b86d1bc8e919a3464e
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-09-10 14:41:26 -0500

    add details to coding challenge for 2024-09-12

---
+++ cc-2024-09-12/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+/*
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+*/
+
+// P: an array of numbers
+// R: the average of the numbers in the array,
+//    0 if the array is empty
+// E: console.log(findAverage([1, 1, 1]), 1);
+//    console.log(findAverage([1, 2, 3]), 2);
+//    console.log(findAverage([]), 0);
+// P:
+
+// create a function that takes in an array of numbers
+function findAverage(arr) {
+
+}
