d01628d720527cfcdd0e5af46b4e5ab234227405
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-09-11 14:50:56 -0500

    add attempt to coding challenge for 2024-09-12

--- cc-2024-09-12/js/main.js
+++ cc-2024-09-12/js/main.js
@@ -16,5 +16,8 @@ Note: Empty arrays should return 0.
 
 // create a function that takes in an array of numbers
 function findAverage(arr) {
-
+    // check if the array is empty,
+    // use reduce to sum the numbers and then divide by the array length to get the average,
+    // return 0 if the array is empty
+    return arr.length > 0 ? arr.reduce((acc, c) => acc + c) / arr.length : 0;
 }
