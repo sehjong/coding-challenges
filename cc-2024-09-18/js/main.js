5a7948cc2c08dd80c631a13876d04a23cc3894ec
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-09-16 16:59:14 -0500

    add details to coding challenge for 2024-09-18

---
+++ cc-2024-09-18/js/main.js
@@ -0,0 +1,35 @@
+// PREP
+
+/*
+Write a function that will check if two given characters are the same case.
+
+If either of the characters is not a letter, return -1
+If both characters are the same case, return 1
+If both characters are letters, but not the same case, return 0
+Examples
+'a' and 'g' returns 1
+
+'A' and 'C' returns 1
+
+'b' and 'G' returns 0
+
+'B' and 'g' returns 0
+
+'0' and '?' returns -1
+*/
+
+// P: characters char1 and char2, which can be letters, numbers, or symbols
+// R: 1 if both characters are letters and they have the same case,
+//    0 if both characters are letters but have different cases,
+//    -1 if either or both characters are not letters
+// E: console.log(checkCase('a', 'g'), 1);
+//    console.log(checkCase('A', 'C'), 1);
+//    console.log(checkCase('b', 'G'), 0);
+//    console.log(checkCase('B', 'g'), 0);
+//    console.log(checkCase('0', '?'), -1);
+// P:
+
+// create a function that takes in characters char1 and char2, which can be letters, numbers, or symbols
+function checkCase(char1, char2) {
+    
+}
