95089291a4a9eb7553aac80104b380891fe3de37
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-09-19 11:02:41 -0500

    add details to coding challenge for 2024-09-21

---
+++ cc-2024-09-21/js/main.js
@@ -0,0 +1,27 @@
+// PREP
+
+/*
+In this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+*/
+
+// P: a number
+// R: true if even and false if odd,
+//    floats with a non-zero decimal value are considered odd for this challenge
+// E: console.log(isEvenNumber(2), true);
+//    console.log(isEvenNumber(0.5), false);
+//    console.log(isEvenNumber(-3), false);
+//    console.log(isEvenNumber(-2), true);
+// P:
+
+// create a function that takes in a number
+function isEvenNumber(n) {
+
+}
