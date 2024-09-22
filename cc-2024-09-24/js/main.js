6dde170caa987709f04e8427fda20df72d106cc4
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-09-22 12:34:48 -0500

    add details to coding challenge for 2024-09-24

---
+++ cc-2024-09-24/js/main.js
@@ -0,0 +1,15 @@
+// PREP
+
+/*
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
+*/
+
+// P: a string
+// R: the string with the first and last characters removed
+// E: console.log(removeChar('abc'), 'b');
+// P:
+
+// create a function that takes in a string
+function removeChar(str) {
+
+}
