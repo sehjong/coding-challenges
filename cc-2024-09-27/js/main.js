700da0da2398dfe9f39f057350655985e2ddaf36
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-09-25 12:56:05 -0500

    add details to coding challenge for 2024-09-27

---
+++ cc-2024-09-27/js/main.js
@@ -0,0 +1,20 @@
+// PREP
+
+/*
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+Examples (Input -> Output)
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+*/
+
+// P: an array
+// R: the array with elements in reverse order
+// E: [1, 2, 3, 4]  -> [4, 3, 2, 1]
+//    [9, 2, 0, 7]  -> [7, 0, 2, 9]
+// P:
+
+// create a function that takes in an array
+function reverseList(arr) {
+    
+}
