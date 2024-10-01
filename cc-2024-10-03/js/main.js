81aefd2598deb9304dd4a13fa9cc408c795699f2
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-10-01 11:18:29 -0500

    add details to coding challenge for 2024-10-03

---
+++ cc-2024-10-03/js/main.js
@@ -0,0 +1,30 @@
+// PREP
+
+/*
+Task
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Mind the input validation.
+
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+Input validation
+If an empty value ( null, None, Nothing, nil etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+*/
+
+// P: an array of numbers
+// R: the sum of all the numbers except the highest and lowest values
+// E: console.log(sumArray([6, 2, 1, 8, 10]), 16);
+//    console.log(sumArray([1, 1, 11, 2, 3 ]), 6);
+//    console.log(sumArray([null]), 0);
+//    console.log(sumArray([ ]), 0);
+//    console.log(sumArray([1]), 0);
+// P:
+
+// create a function that takes in an array of numbers
+function sumArray(array) {
+
+}
