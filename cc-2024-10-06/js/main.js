448a0ba6a06efefbb64cc1dac3b6884fc46a4e5c
Sehjong Hamjong <sehjong.hamjong@gmail.com>
2024-10-04 11:56:21 -0500

    add details to coding challenge for 2024-10-06

---
+++ cc-2024-10-06/js/main.js
@@ -0,0 +1,17 @@
+// PREP
+
+/*
+In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+
+For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+*/
+
+// P: two numbers, a value, and a maximum boundry for its multiples
+// R: an array of numbers that are multiples of the given number, up to and including the maximum boundry
+// E: (2, 6) -> [2, 4, 6]
+// P:
+
+// create a function that takes in two numbers, a value, and a maximum boundry for its multiples
+function findMultiples(val, limit) {
+
+}
